621388e34a342e7aac12e47326271b5f75259515
Eva <ehamrud@gmail.com>
2022-02-07 19:20:11 +0000

    sex regression

--- NF-downstream_analysis/bin/3_gene_activity.R
+++ NF-downstream_analysis/bin/3_gene_activity.R
@@ -110,7 +110,7 @@ graphics.off()
 
 
 ###########################################################################################################
-######################################## GEX SCALING #####################################################
+######################################## Top variable genes ###############################################
 
 seurat <- FindVariableFeatures(seurat, selection.method = "vst", nfeatures = 2000)
  
@@ -125,11 +125,6 @@ png(paste0(plot_path, 'MostVariable.png'), height = 15, width = 20, units = 'cm'
 plot2
 graphics.off()
 
-###########################################################################################################
-######################################## SEX FILTERING #####################################################
- 
-DimPlot(seurat, reduction = "umap")
- 
 markers <- FindAllMarkers(seurat, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 markers %>%
   group_by(cluster) %>%
@@ -139,6 +134,9 @@ png(paste0(plot_path, 'top_markers.png'), height = 10, width = 20, units = 'cm',
 grid.arrange(top=textGrob("Top Cluster Markers", gp=gpar(fontsize=12, fontface = "bold"), hjust = 0.5, vjust = 3),
              tableGrob(top10, rows=NULL, theme = ttheme_minimal()))
 graphics.off()
+
+###########################################################################################################
+######################################## SEX FILTERING #####################################################
 
 png(paste0(plot_path, 'FeaturePlot_sex_genes.png'), height = 20, width = 30, units = 'cm', res = 400)
 FeaturePlot(
@@ -150,108 +148,96 @@ FeaturePlot(
 )
 graphics.off()
 
-# #### need to remove MT and sex genes (will later regress properly)
-# names_sex_genes_W <- rownames(markers)[grepl("^W", rownames(markers))]
-# names_sex_genes_Z <- rownames(markers)[grepl("^Z", rownames(markers))]
-# 
-# sex_genes <- grepl("^W|^Z", rownames(markers))
-# markers_filt <- markers[!sex_genes,]
-# markers_filt %>%
-#   group_by(cluster) %>%
-#   top_n(n = 10, wt = avg_log2FC) -> top10
-# DoHeatmap(signac_rna, features = top10$gene) + NoLegend()
-# 
+# Use W chromosome genes to K-means cluster the cells into male (zz) and female (zw)
+W_genes <- as.matrix(seurat@assays$RNA[grepl("W-", rownames(seurat@assays$RNA)),])
+k_clusters <- kmeans(t(W_genes), 2)
+k_clusters <- data.frame(k_clusters$cluster)
+seurat@meta.data$k_clusters <- k_clusters[match(colnames(seurat@assays$RNA), rownames(k_clusters)),]
 
+# Get rownames for kmeans clusters 1 and 2
+k_clus_1 <- rownames(seurat@meta.data[seurat@meta.data$k_clusters == 1,])
+k_clus_2 <- rownames(seurat@meta.data[seurat@meta.data$k_clusters == 2,])
+ 
+# K clustering identities are stochastic, so I need to identify which cluster is male and female
+# Sum of W genes is order of magnitude greater in cluster 2 - these are the female cells
+sumclus1 <- sum(W_genes[,k_clus_1])
+sumclus2 <- sum(W_genes[,k_clus_2])
+ 
+if(sumclus1 < sumclus2){
+  k_male <- k_clus_1
+  k_female <- k_clus_2
+} else {
+  k_female <- k_clus_1
+  k_male <- k_clus_2
+}
+ 
+# Add sex data to meta.data
+seurat@meta.data$sex <- unlist(lapply(rownames(seurat@meta.data), function(x)
+  if(x %in% k_male){"male"} else if(x %in% k_female){"female"} else{stop("cell sex is not assigned")}))
 
+png(paste0(plot_path,"UMAP_sex.png"), height = 18, width = 18, units = "cm", res = 200)
+DimPlot(seurat, group.by = "sex")
+graphics.off()
 
-# 
-# # Use W chromosome genes to K-means cluster the cells into male (zz) and female (zw)
-# W_genes <- as.matrix(signac_rna@assays$RNA[grepl("W-", rownames(signac_rna@assays$RNA)),])
-# k_clusters <- kmeans(t(W_genes), 2)
-# k_clusters <- data.frame(k_clusters$cluster)
-# signac_rna@meta.data$k_clusters <- k_clusters[match(colnames(signac_rna@assays$RNA), rownames(k_clusters)),]
-# 
-# # Get rownames for kmeans clusters 1 and 2
-# k_clus_1 <- rownames(signac_rna@meta.data[signac_rna@meta.data$k_clusters == 1,])
-# k_clus_2 <- rownames(signac_rna@meta.data[signac_rna@meta.data$k_clusters == 2,])
-# 
-# # K clustering identities are stochastic, so I need to identify which cluster is male and female
-# # Sum of W genes is order of magnitude greater in cluster 2 - these are the female cells
-# sumclus1 <- sum(W_genes[,k_clus_1])
-# sumclus2 <- sum(W_genes[,k_clus_2])
-# 
-# if(sumclus1 < sumclus2){
-#   k_male <- k_clus_1
-#   k_female <- k_clus_2
-# } else {
-#   k_female <- k_clus_1
-#   k_male <- k_clus_2
-# }
-# 
-# # Add sex data to meta.data
-# signac_rna@meta.data$sex <- unlist(lapply(rownames(signac_rna@meta.data), function(x)
-#   if(x %in% k_male){"male"} else if(x %in% k_female){"female"} else{stop("cell sex is not assigned")}))
-# 
-# 
-# # Make dataframe for mean Z expression in male cells
-# mean_Z_male <- data.frame(Z.mean = apply(signac_rna@assays$RNA[grepl("Z-", rownames(signac_rna@assays$RNA)), k_male], 1, mean))
-# # add 1 before log2 as log2(1) = 0
-# mean_Z_male <- log2(mean_Z_male + 1)
-# 
-# # Make dataframe for mean Z expression in female cells
-# mean_Z_female <- data.frame(Z.mean = apply(signac_rna@assays$RNA[grepl("Z-", rownames(signac_rna@assays$RNA)), k_female], 1, mean))
-# mean_Z_female <- log2(mean_Z_female + 1)
-# 
-# # Make dataframe for mean autosomal expression in male cells
-# mean_auto_male <- data.frame(auto.mean = apply(signac_rna@assays$RNA[!grepl("Z-", rownames(signac_rna@assays$RNA)) & !grepl("W-", rownames(signac_rna@assays$RNA)), k_male], 1, mean))
-# mean_auto_male <- log2(mean_auto_male + 1)
-# 
-# # Make dataframe for mean autosomal expression in male cells
-# mean_auto_female <- data.frame(auto.mean = apply(signac_rna@assays$RNA[!grepl("Z-", rownames(signac_rna@assays$RNA)) & !grepl("W-", rownames(signac_rna@assays$RNA)), k_female], 1, mean))
-# mean_auto_female <- log2(mean_auto_female + 1)
-# 
-# # Calculate FC by subtracting log2 expression from each other
-# FC <- list()
-# FC$Z <- mean_Z_male - mean_Z_female
-# FC$auto <-  mean_auto_male - mean_auto_female
-# 
-# # Plot boxplot of Z gene and autosomal expression in male vs female cells
-# png(paste0(plot_path,"sex_kmeans_log2FC_boxplot.png"), height = 18, width = 18, units = "cm", res = 200)
-# boxplot(c(FC$Z, FC$auto),  ylab = "male - female log2 FC (mean normalised UMI +1)", names = c("Z chromosome genes", "autosomal genes"))
-# graphics.off()
-# 
-# signac_rna_sex_filt <- signac_rna
-# 
-# DefaultAssay(signac_rna_sex_filt) <- "RNA"
-# 
-# signac_rna_sex_filt <- ScaleData(signac_rna_sex_filt, features = rownames(signac_rna_sex_filt), vars.to.regress = c("sex"), verbose = TRUE)
-# 
-# signac_rna_sex_filt <- RunPCA(object = signac_rna_sex_filt, verbose = FALSE)
-# signac_rna_sex_filt <- FindNeighbors(signac_rna_sex_filt, dims = 1:10, verbose = FALSE)
-# signac_rna_sex_filt <- RunUMAP(signac_rna_sex_filt, dims = 1:10, verbose = FALSE)
-# signac_rna_sex_filt <- FindClusters(signac_rna_sex_filt, resolution = 0.5, verbose = FALSE)
-# 
-# saveRDS(signac_rna_sex_filt, paste0(plot_path, "signac_filtered_GeneActivity_sexfilt.RDS"))
-# 
-# ###########################################################################################################
-# ######################################## MT REGRESS #####################################################
-# 
-# signac_rna_sex_mt_filt <- readRDS(paste0(plot_path, "signac_filtered_GeneActivity_sexfilt.RDS"))
-# DefaultAssay(signac_rna_sex_mt_filt) <- 'RNA'
-# 
-# markers <- FindAllMarkers(signac_rna_sex_mt_filt, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-# names_mt_genes <- rownames(markers)[grepl("^MT", rownames(markers))]
-# 
-# signac_rna_sex_mt_filt <- PercentageFeatureSet(signac_rna_sex_mt_filt, pattern = "^MT-", col.name = "percent.mt")
-# signac_rna_sex_mt_filt <- ScaleData(signac_rna_sex_mt_filt, features = rownames(signac_rna_sex_mt_filt), vars.to.regress = c("percent.mt", "sex"))
-# 
-# signac_rna_sex_mt_filt <- RunPCA(object = signac_rna_sex_mt_filt, verbose = FALSE)
-# signac_rna_sex_mt_filt <- FindNeighbors(signac_rna_sex_mt_filt, dims = 1:10, verbose = FALSE)
-# signac_rna_sex_mt_filt <- RunUMAP(signac_rna_sex_mt_filt, dims = 1:10, verbose = FALSE)
-# signac_rna_sex_mt_filt <- FindClusters(signac_rna_sex_mt_filt, resolution = 0.5, verbose = FALSE)
-# 
-# saveRDS(signac_rna_sex_mt_filt, paste0(plot_path, "signac_filtered_GeneActivity_sexmtfilt.RDS"))
-# 
+# Make dataframe for mean Z expression in male cells
+mean_Z_male <- data.frame(Z.mean = apply(seurat@assays$RNA[grepl("Z-", rownames(seurat@assays$RNA)), k_male], 1, mean))
+# add 1 before log2 as log2(1) = 0
+mean_Z_male <- log2(mean_Z_male + 1)
+
+# Make dataframe for mean Z expression in female cells
+mean_Z_female <- data.frame(Z.mean = apply(seurat@assays$RNA[grepl("Z-", rownames(seurat@assays$RNA)), k_female], 1, mean))
+mean_Z_female <- log2(mean_Z_female + 1)
+
+# Make dataframe for mean autosomal expression in male cells
+mean_auto_male <- data.frame(auto.mean = apply(seurat@assays$RNA[!grepl("Z-", rownames(seurat@assays$RNA)) & !grepl("W-", rownames(seurat@assays$RNA)), k_male], 1, mean))
+mean_auto_male <- log2(mean_auto_male + 1)
+
+# Make dataframe for mean autosomal expression in male cells
+mean_auto_female <- data.frame(auto.mean = apply(seurat@assays$RNA[!grepl("Z-", rownames(seurat@assays$RNA)) & !grepl("W-", rownames(seurat@assays$RNA)), k_female], 1, mean))
+mean_auto_female <- log2(mean_auto_female + 1)
+ 
+# Calculate FC by subtracting log2 expression from each other
+FC <- list()
+FC$Z <- mean_Z_male - mean_Z_female
+FC$auto <-  mean_auto_male - mean_auto_female
+ 
+# Plot boxplot of Z gene and autosomal expression in male vs female cells
+png(paste0(plot_path,"sex_kmeans_log2FC_boxplot.png"), height = 18, width = 18, units = "cm", res = 200)
+boxplot(c(FC$Z, FC$auto),  ylab = "male - female log2 FC (mean normalised UMI +1)", names = c("Z chromosome genes", "autosomal genes"))
+graphics.off()
+
+DefaultAssay(seurat) <- 'peaks'
+seurat_sex_filt <- ScaleData(seurat, vars.to.regress = c("sex"), verbose = TRUE)
+
+seurat_sex_filt <- RunUMAP(object = seurat_sex_filt, reduction = 'lsi', dims = 2:30)
+seurat_sex_filt <- FindNeighbors(object = seurat_sex_filt, reduction = 'lsi', dims = 2:30)
+seurat_sex_filt <- FindClusters(object = seurat_sex_filt, verbose = FALSE, algorithm = 3)
+
+png(paste0(plot_path,"UMAP_sex_regressed.png"), height = 18, width = 18, units = "cm", res = 200)
+DimPlot(seurat, group.by = "sex")
+graphics.off()
+
+saveRDS(signac_rna_sex_filt, paste0(rds_path, "seurat_GeneActivity_sexfilt.RDS"))
+
+###########################################################################################################
+######################################## MT REGRESS #####################################################
+
+signac_rna_sex_mt_filt <- readRDS(paste0(plot_path, "signac_filtered_GeneActivity_sexfilt.RDS"))
+DefaultAssay(signac_rna_sex_mt_filt) <- 'RNA'
+
+markers <- FindAllMarkers(signac_rna_sex_mt_filt, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+names_mt_genes <- rownames(markers)[grepl("^MT", rownames(markers))]
+
+signac_rna_sex_mt_filt <- PercentageFeatureSet(signac_rna_sex_mt_filt, pattern = "^MT-", col.name = "percent.mt")
+signac_rna_sex_mt_filt <- ScaleData(signac_rna_sex_mt_filt, features = rownames(signac_rna_sex_mt_filt), vars.to.regress = c("percent.mt", "sex"))
+
+signac_rna_sex_mt_filt <- RunPCA(object = signac_rna_sex_mt_filt, verbose = FALSE)
+signac_rna_sex_mt_filt <- FindNeighbors(signac_rna_sex_mt_filt, dims = 1:10, verbose = FALSE)
+signac_rna_sex_mt_filt <- RunUMAP(signac_rna_sex_mt_filt, dims = 1:10, verbose = FALSE)
+signac_rna_sex_mt_filt <- FindClusters(signac_rna_sex_mt_filt, resolution = 0.5, verbose = FALSE)
+
+saveRDS(signac_rna_sex_mt_filt, paste0(plot_path, "signac_filtered_GeneActivity_sexmtfilt.RDS"))
+
 # ###########################################################################################################
 # ######################################## VISUALISATIONS #####################################################
 # 
