be70dcceecb1141338a6333cf922ab9fe3c203c7
Eva <ehamrud@gmail.com>
2022-03-02 18:08:05 +0000

    add archr visusalisation script to workflow

---
+++ NF-downstream_analysis/bin/3_ArchR_visualisations.R
@@ -0,0 +1,444 @@
+#!/usr/bin/env Rscript
+
+print("3_visualisations_ArchR")
+
+############################## Load libraries #######################################
+library(getopt)
+library(future)
+library(tidyverse)
+library(grid)
+library(gridExtra)
+library(clustree)
+library(GenomeInfoDb)
+library(ggplot2)
+library(dplyr)
+library(rtracklayer)
+library(GenomicRanges)
+library(GenomicFeatures)
+library(parallel)
+library(ArchR)
+library(GenomicFeatures)
+
+############################## Set up script options #######################################
+spec = matrix(c(
+  'runtype', 'l', 2, "character",
+  'cores'   , 'c', 2, "integer"
+), byrow=TRUE, ncol=4)
+opt = getopt(spec)
+
+# Set paths and load data
+{
+  if(length(commandArgs(trailingOnly = TRUE)) == 0){
+    cat('No command line arguments provided, paths are set for running interactively in Rstudio server\n')
+    
+    setwd("~/NF-downstream_analysis")
+    ncores = 8
+    
+    plot_path = "../output/NF-downstream_analysis/3_ArchR_visualisations/plots/"
+    rds_path = "../output/NF-downstream_analysis/3_ArchR_visualisations/rds_files/"
+    data_path = "../output/NF-downstream_analysis/2_ArchR_filtering/"
+    
+  } else if (opt$runtype == "nextflow"){
+    cat('pipeline running through Nextflow\n')
+    
+    plot_path = "./plots/"
+    rds_path = "./rds_files/"
+    data_path = "./input/"
+    ncores = opt$cores
+    
+    # Multi-core when running from command line
+    plan("multicore", workers = ncores)
+    options(future.globals.maxSize = 155* 1024^3)
+    addArchRThreads(threads = ncores) 
+    
+  } else {
+    stop("--runtype must be set to 'nextflow'")
+  }
+  
+  cat(paste0("script ran with ", ncores, " cores\n"))
+  dir.create(plot_path, recursive = T)
+  dir.create(rds_path, recursive = T)
+}
+
+addArchRThreads(threads = 16) 
+
+############################## Function to split samples #################################
+
+## need to make this generic (not just split by stage) need to overcome passing variable to $ issue
+split_ArchR_by_stage <- function(ArchR_project){
+  split_names <- unique(ArchR_project$stage)
+  split_ArchR <- c()
+  for (i in split_names){
+    idxSample <- BiocGenerics::which(ArchR_project$stage %in% i)
+    cellsSample <- ArchR_project$cellNames[idxSample]
+    split_ArchR <- c(split_ArchR, ArchR_project[cellsSample, ])
+  }
+  return(split_ArchR)
+}
+
+## add a function here to extract top differentially expressed genes per cluster
+
+############################## Read in ArchR project #######################################
+ArchR <- loadArchRProject(path = paste0(data_path, "./rds_files/Save-ArchR"), force = FALSE, showLogo = TRUE)
+paste0("Memory Size = ", round(object.size(ArchR) / 10^6, 3), " MB")
+
+# ################################################################################
+# ############################## Filtering #######################################
+# 
+# ############################## Split samples #################################
+# stage_ArchR <- split_ArchR_by_stage(ArchR)
+# names(stage_ArchR) <- unique(ArchR_project$stage)
+# 
+# ############################## Filter on different thresholds? #################################
+# # would maybe increase minimum frag count for hh7 and maybe ss4? do I want to do that?
+# # other thing is maybe increasing max frag count for hh7 but that needs to be done in script 1
+# 
+# ############################## Filter doublets #################################
+# ArchR_filtered <- filterDoublets(ArchR)
+# 
+
+
+############################## Iterative LSI Dimensionality Reduction #################################
+ArchR <- addIterativeLSI(
+  ArchRProj = ArchR,
+  useMatrix = "TileMatrix", 
+  name = "IterativeLSI", 
+  iterations = 2, 
+  clusterParams = list( #See Seurat::FindClusters
+    resolution = c(0.2), 
+    sampleCells = 10000, 
+    n.start = 10
+  ), 
+  varFeatures = 25000, 
+  dimsToUse = 1:30
+)
+
+############################## Seurat graph-based clustering #################################
+ArchR <- addClusters(
+  input = ArchR,
+  reducedDims = "IterativeLSI",
+  method = "Seurat",
+  name = "Clusters",
+  resolution = 2
+)
+print(table(ArchR$Clusters))
+# add a barchart here of cell numbers in each cluster?
+
+############################## Which samples in which clusters #################################
+cM <- confusionMatrix(paste0(ArchR$Clusters), paste0(ArchR$stage))
+cM <- cM / Matrix::rowSums(cM)
+p <- pheatmap::pheatmap(
+  mat = as.matrix(cM), 
+  color = paletteContinuous("whiteBlue"), 
+  border_color = "black"
+)
+
+png(paste0(plot_path, "Cluster_stage_distribution.png"), width=25, height=20, units = 'cm', res = 200)
+print(p)
+graphics.off()
+
+############################## Run and plot UMAP #################################
+ArchR <- addUMAP(
+  ArchRProj = ArchR, 
+  reducedDims = "IterativeLSI", 
+  name = "UMAP", 
+  nNeighbors = 30, 
+  minDist = 0.5, 
+  metric = "cosine"
+)
+
+p1 <- plotEmbedding(ArchRProj = ArchR, colorBy = "cellColData", name = "stage", embedding = "UMAP")
+p2 <- plotEmbedding(ArchRProj = ArchR, colorBy = "cellColData", name = "Clusters", embedding = "UMAP")
+
+png(paste0(plot_path, "UMAPs.png"), width=60, height=40, units = 'cm', res = 200)
+ggAlignPlots(p1, p2, type = "h")
+graphics.off()
+
+paste0("Memory Size = ", round(object.size(ArchR) / 10^6, 3), " MB")
+saveArchRProject(ArchRProj = full_data, outputDirectory = paste0(rds_path, "full_data"), load = FALSE)
+
+############################## Run on separate stages and save RDS files #################################
+stage_ArchR <- split_ArchR_by_stage(ArchR)
+names(stage_ArchR) <- unique(ArchR$stage)
+
+## ss8
+ss8 <- stage_ArchR$ss8
+ss8 <- addIterativeLSI(ArchRProj = ss8, useMatrix = "TileMatrix", name = "IterativeLSI", iterations = 2, 
+  clusterParams = list( #See Seurat::FindClusters
+    resolution = c(0.2), 
+    sampleCells = 10000, 
+    n.start = 10
+  ), varFeatures = 25000, dimsToUse = 1:30, force = TRUE)
+ss8 <- addClusters(input = ss8, reducedDims = "IterativeLSI", method = "Seurat",
+  name = "Clusters", resolution = 2, force = TRUE)
+ss8 <- addUMAP(ArchRProj = ss8, reducedDims = "IterativeLSI", name = "UMAP", 
+  nNeighbors = 30, minDist = 0.5, metric = "cosine", force = TRUE)
+p <- plotEmbedding(ArchRProj = ss8, colorBy = "cellColData", name = "Clusters", embedding = "UMAP")
+png(paste0(plot_path, "UMAP_ss8.png"), width=20, height=20, units = 'cm', res = 200)
+print(p)
+graphics.off()
+
+paste0("Memory Size = ", round(object.size(ss8) / 10^6, 3), " MB")
+saveArchRProject(ArchRProj = ss8, outputDirectory = paste0(rds_path, "ss8"), load = FALSE)
+
+## ss4
+ss4 <- stage_ArchR$ss4
+ss4 <- addIterativeLSI(ArchRProj = ss4, useMatrix = "TileMatrix", name = "IterativeLSI", iterations = 2, 
+                       clusterParams = list( #See Seurat::FindClusters
+                         resolution = c(0.2), 
+                         sampleCells = 10000, 
+                         n.start = 10
+                       ), varFeatures = 25000, dimsToUse = 1:30, force = TRUE)
+ss4 <- addClusters(input = ss4, reducedDims = "IterativeLSI", method = "Seurat",
+                   name = "Clusters", resolution = 2, force = TRUE)
+ss4 <- addUMAP(ArchRProj = ss4, reducedDims = "IterativeLSI", name = "UMAP", 
+               nNeighbors = 30, minDist = 0.5, metric = "cosine", force = TRUE)
+p <- plotEmbedding(ArchRProj = ss4, colorBy = "cellColData", name = "Clusters", embedding = "UMAP")
+png(paste0(plot_path, "UMAP_ss4.png"), width=20, height=20, units = 'cm', res = 200)
+print(p)
+graphics.off()
+
+paste0("Memory Size = ", round(object.size(ss4) / 10^6, 3), " MB")
+saveArchRProject(ArchRProj = ss4, outputDirectory = paste0(rds_path, "ss4"), load = FALSE)
+
+## HH7
+HH7 <- stage_ArchR$HH7
+HH7 <- addIterativeLSI(ArchRProj = HH7, useMatrix = "TileMatrix", name = "IterativeLSI", iterations = 2, 
+                       clusterParams = list( #See Seurat::FindClusters
+                         resolution = c(0.2), 
+                         sampleCells = 10000, 
+                         n.start = 10
+                       ), varFeatures = 25000, dimsToUse = 1:30, force = TRUE)
+HH7 <- addClusters(input = HH7, reducedDims = "IterativeLSI", method = "Seurat",
+                   name = "Clusters", resolution = 2, force = TRUE)
+HH7 <- addUMAP(ArchRProj = HH7, reducedDims = "IterativeLSI", name = "UMAP", 
+               nNeighbors = 30, minDist = 0.5, metric = "cosine", force = TRUE)
+p <- plotEmbedding(ArchRProj = HH7, colorBy = "cellColData", name = "Clusters", embedding = "UMAP")
+png(paste0(plot_path, "UMAP_HH7.png"), width=20, height=20, units = 'cm', res = 200)
+print(p)
+graphics.off()
+
+paste0("Memory Size = ", round(object.size(HH7) / 10^6, 3), " MB")
+saveArchRProject(ArchRProj = HH7, outputDirectory = paste0(rds_path, "HH7"), load = FALSE)
+
+## HH6
+HH6 <- stage_ArchR$HH6
+HH6 <- addIterativeLSI(ArchRProj = HH6, useMatrix = "TileMatrix", name = "IterativeLSI", iterations = 2, 
+                       clusterParams = list( #See Seurat::FindClusters
+                         resolution = c(0.2), 
+                         sampleCells = 10000, 
+                         n.start = 10
+                       ), varFeatures = 25000, dimsToUse = 1:30, force = TRUE)
+HH6 <- addClusters(input = HH6, reducedDims = "IterativeLSI", method = "Seurat",
+                   name = "Clusters", resolution = 2, force = TRUE)
+HH6 <- addUMAP(ArchRProj = HH6, reducedDims = "IterativeLSI", name = "UMAP", 
+               nNeighbors = 30, minDist = 0.5, metric = "cosine", force = TRUE)
+p <- plotEmbedding(ArchRProj = HH6, colorBy = "cellColData", name = "Clusters", embedding = "UMAP")
+png(paste0(plot_path, "UMAP_HH6.png"), width=20, height=20, units = 'cm', res = 200)
+print(p)
+graphics.off()
+
+paste0("Memory Size = ", round(object.size(HH6) / 10^6, 3), " MB")
+saveArchRProject(ArchRProj = HH6, outputDirectory = paste0(rds_path, "HH6"), load = FALSE)
+
+## HH5
+HH5 <- stage_ArchR$HH5
+HH5 <- addIterativeLSI(ArchRProj = HH5, useMatrix = "TileMatrix", name = "IterativeLSI", iterations = 2, 
+                       clusterParams = list( #See Seurat::FindClusters
+                         resolution = c(0.2), 
+                         sampleCells = 10000, 
+                         n.start = 10
+                       ), varFeatures = 25000, dimsToUse = 1:30, force = TRUE)
+HH5 <- addClusters(input = HH5, reducedDims = "IterativeLSI", method = "Seurat",
+                   name = "Clusters", resolution = 2, force = TRUE)
+HH5 <- addUMAP(ArchRProj = HH5, reducedDims = "IterativeLSI", name = "UMAP", 
+               nNeighbors = 30, minDist = 0.5, metric = "cosine", force = TRUE)
+p <- plotEmbedding(ArchRProj = HH5, colorBy = "cellColData", name = "Clusters", embedding = "UMAP")
+png(paste0(plot_path, "UMAP_HH5.png"), width=20, height=20, units = 'cm', res = 200)
+print(p)
+graphics.off()
+
+paste0("Memory Size = ", round(object.size(HH5) / 10^6, 3), " MB")
+saveArchRProject(ArchRProj = HH5, outputDirectory = paste0(rds_path, "HH5"), load = FALSE)
+
+############################## Calculate top gene markers and plot #################################
+
+# all
+markers <- getMarkerFeatures(
+  ArchRProj = ArchR, 
+  useMatrix = "GeneScoreMatrix", 
+  groupBy = "Clusters",
+  bias = c("TSSEnrichment", "log10(nFrags)"),
+  testMethod = "wilcoxon"
+)
+markerList <- getMarkers(markers, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
+top_markers <- tibble()
+for (i in 1:length(markerList)){
+  table <- as.tibble(markerList[[i]]) 
+  table <- table %>% top_n(5, Log2FC) %>% mutate(cluster = i)
+  top_markers <- rbind(top_markers, table)
+}
+png(paste0(plot_path, 'top_genes.png'), height = 40, width = 30, units = 'cm', res = 400)
+grid.arrange(tableGrob(top_markers))
+dev.off()
+
+markerGenes <- top_markers$name
+heatmap <- markerHeatmap(
+  seMarker = markers, 
+  cutOff = "FDR <= 0.01 & Log2FC >= 1.25", 
+  transpose = TRUE,
+  labelMarkers = markerGenes
+)
+png(paste0(plot_path, 'heatmap.png'), height = 30, width = 40, units = 'cm', res = 400)
+ComplexHeatmap::draw(heatmap, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+graphics.off()
+
+# ss8
+markers <- getMarkerFeatures(
+  ArchRProj = ss8, 
+  useMatrix = "GeneScoreMatrix", 
+  groupBy = "Clusters",
+  bias = c("TSSEnrichment", "log10(nFrags)"),
+  testMethod = "wilcoxon"
+)
+markerList <- getMarkers(markers, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
+top_markers <- tibble()
+for (i in 1:length(markerList)){
+  table <- as.tibble(markerList[[i]]) 
+  table <- table %>% top_n(5, Log2FC) %>% mutate(cluster = i)
+  top_markers <- rbind(top_markers, table)
+}
+png(paste0(plot_path, 'ss8_top_genes.png'), height = 40, width = 30, units = 'cm', res = 400)
+grid.arrange(tableGrob(top_markers))
+dev.off()
+
+markerGenes <- top_markers$name
+heatmap <- markerHeatmap(
+  seMarker = markers, 
+  cutOff = "FDR <= 0.01 & Log2FC >= 1.25", 
+  transpose = TRUE,
+  labelMarkers = markerGenes
+)
+png(paste0(plot_path, 'ss8_heatmap.png'), height = 30, width = 40, units = 'cm', res = 400)
+ComplexHeatmap::draw(heatmap, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+graphics.off()
+
+# ss4
+markers <- getMarkerFeatures(
+  ArchRProj = ss4, 
+  useMatrix = "GeneScoreMatrix", 
+  groupBy = "Clusters",
+  bias = c("TSSEnrichment", "log10(nFrags)"),
+  testMethod = "wilcoxon"
+)
+markerList <- getMarkers(markers, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
+top_markers <- tibble()
+for (i in 1:length(markerList)){
+  table <- as.tibble(markerList[[i]]) 
+  table <- table %>% top_n(5, Log2FC) %>% mutate(cluster = i)
+  top_markers <- rbind(top_markers, table)
+}
+png(paste0(plot_path, 'ss4_top_genes.png'), height = 40, width = 30, units = 'cm', res = 400)
+grid.arrange(tableGrob(top_markers))
+dev.off()
+
+markerGenes <- top_markers$name
+heatmap <- markerHeatmap(
+  seMarker = markers, 
+  cutOff = "FDR <= 0.01 & Log2FC >= 1.25", 
+  transpose = TRUE,
+  labelMarkers = markerGenes
+)
+png(paste0(plot_path, 'ss4_heatmap.png'), height = 30, width = 40, units = 'cm', res = 400)
+ComplexHeatmap::draw(heatmap, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+graphics.off()
+
+# HH7
+markers <- getMarkerFeatures(
+  ArchRProj = HH7, 
+  useMatrix = "GeneScoreMatrix", 
+  groupBy = "Clusters",
+  bias = c("TSSEnrichment", "log10(nFrags)"),
+  testMethod = "wilcoxon"
+)
+markerList <- getMarkers(markers, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
+top_markers <- tibble()
+for (i in 1:length(markerList)){
+  table <- as.tibble(markerList[[i]]) 
+  table <- table %>% top_n(5, Log2FC) %>% mutate(cluster = i)
+  top_markers <- rbind(top_markers, table)
+}
+png(paste0(plot_path, 'HH7_top_genes.png'), height = 40, width = 30, units = 'cm', res = 400)
+grid.arrange(tableGrob(top_markers))
+dev.off()
+
+markerGenes <- top_markers$name
+heatmap <- markerHeatmap(
+  seMarker = markers, 
+  cutOff = "FDR <= 0.01 & Log2FC >= 1.25", 
+  transpose = TRUE,
+  labelMarkers = markerGenes
+)
+png(paste0(plot_path, 'HH7_heatmap.png'), height = 30, width = 40, units = 'cm', res = 400)
+ComplexHeatmap::draw(heatmap, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+graphics.off()
+
+# HH6
+markers <- getMarkerFeatures(
+  ArchRProj = HH6, 
+  useMatrix = "GeneScoreMatrix", 
+  groupBy = "Clusters",
+  bias = c("TSSEnrichment", "log10(nFrags)"),
+  testMethod = "wilcoxon"
+)
+markerList <- getMarkers(markers, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
+top_markers <- tibble()
+for (i in 1:length(markerList)){
+  table <- as.tibble(markerList[[i]]) 
+  table <- table %>% top_n(5, Log2FC) %>% mutate(cluster = i)
+  top_markers <- rbind(top_markers, table)
+}
+png(paste0(plot_path, 'HH6_top_genes.png'), height = 40, width = 30, units = 'cm', res = 400)
+grid.arrange(tableGrob(top_markers))
+dev.off()
+
+markerGenes <- top_markers$name
+heatmap <- markerHeatmap(
+  seMarker = markers, 
+  cutOff = "FDR <= 0.01 & Log2FC >= 1.25", 
+  transpose = TRUE,
+  labelMarkers = markerGenes
+)
+png(paste0(plot_path, 'HH6_heatmap.png'), height = 30, width = 40, units = 'cm', res = 400)
+ComplexHeatmap::draw(heatmap, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+graphics.off()
+
+# HH5
+markers <- getMarkerFeatures(
+  ArchRProj = HH5, 
+  useMatrix = "GeneScoreMatrix", 
+  groupBy = "Clusters",
+  bias = c("TSSEnrichment", "log10(nFrags)"),
+  testMethod = "wilcoxon"
+)
+markerList <- getMarkers(markers, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
+top_markers <- tibble()
+for (i in 1:length(markerList)){
+  table <- as.tibble(markerList[[i]]) 
+  table <- table %>% top_n(5, Log2FC) %>% mutate(cluster = i)
+  top_markers <- rbind(top_markers, table)
+}
+png(paste0(plot_path, 'HH5_top_genes.png'), height = 40, width = 30, units = 'cm', res = 400)
+grid.arrange(tableGrob(top_markers))
+dev.off()
+
+markerGenes <- top_markers$name
+heatmap <- markerHeatmap(
+  seMarker = markers, 
+  cutOff = "FDR <= 0.01 & Log2FC >= 1.25", 
+  transpose = TRUE,
+  labelMarkers = markerGenes
+)
+png(paste0(plot_path, 'HH5_heatmap.png'), height = 30, width = 40, units = 'cm', res = 400)
+ComplexHeatmap::draw(heatmap, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+graphics.off()
--- NF-downstream_analysis/subworkflows/local/archr_processing.nf
+++ NF-downstream_analysis/subworkflows/local/archr_processing.nf
@@ -3,6 +3,7 @@ nextflow.enable.dsl = 2
 
 include {R as ARCHR_PREPROCESSING} from "$baseDir/modules/local/r/main"               addParams(script: file("$baseDir/bin/1_ArchR_preprocessing.R", checkIfExists: true) )
 include {R as ARCHR_FILTERING} from "$baseDir/modules/local/r/main"               addParams(script: file("$baseDir/bin/2_ArchR_filtering.R", checkIfExists: true) )
+include {R as ARCHR_VISUALISE} from "$baseDir/modules/local/r/main"               addParams(script: file("$baseDir/bin/3_ArchR_visualisations.R", checkIfExists: true) )
 include {EDIT_GTF} from "$baseDir/modules/local/edit_gtf/main"
 
 workflow ARCHR_PROCESSING {
@@ -22,6 +23,7 @@ workflow ARCHR_PROCESSING {
 
     ARCHR_PREPROCESSING( ch_input_modified )
     ARCHR_FILTERING( ARCHR_PREPROCESSING.out )
+    ARCHR_VISUALISE( ARCHR_FILTERING.out )
 
     //emit:
     //signac_predicted_gex = GEX_FILTERING.out
