66673775c4aeff8f96746bc5be922beb61ea42d3
Eva <ehamrud@gmail.com>
2022-03-11 15:51:44 +0000

    init alignment pipeline - will need to finish

---
+++ run_scripts/test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#SBATCH --job-name=Integrated-npb/NF-cellranger_align
+#SBATCH -t 72:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=eva.hamrud@crick.ac.uk
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/21.10.6
+ml Singularity/3.6.4
+ml Graphviz
+
+export TERM=xterm
+export NXF_VER=21.10.6
+export NXF_SINGULARITY_CACHEDIR=/camp/home/thierya/working/NF_singularity
+
+## UPDATE PIPLINE
+nextflow pull luslab/cheshic-nf-multi-omic
+
+nextflow run luslab/cheshic-nf-multi-omic \
+    -r cellranger \
+    -c ./NF-cellranger_align/conf/crick_params.config \
+    --sample_sheet ./NF-cellranger_align/samplesheet.csv \
+    --outdir ./output/NF-cellranger_align \
+    --email eva.hamrud@crick.ac.uk \
+    -resume
