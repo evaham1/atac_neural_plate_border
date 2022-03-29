dbc6eb7230d0750cea56dcbc157b1ec7bcd3df62
Eva <ehamrud@gmail.com>
2022-03-29 17:32:09 +0100

    typo

--- NF-scRNAseq/bin/subset.R
+++ NF-scRNAseq/bin/subset.R
@@ -44,7 +44,7 @@ if(is.null(opt$groups1)){
 # Set paths and load data
 plot_path = "./plots/"
 rds_path = "./rds_files/"
-data_path = "./input/rds_files/"
+data_path = "./input/"
 
 dir.create(plot_path, recursive = T)
 dir.create(rds_path, recursive = T)
