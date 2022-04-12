fd17a292e36a181aa44477b65ee2aeb8cd8963cd
Eva <ehamrud@gmail.com>
2022-04-12 11:29:28 +0100

    update subset script

--- NF-downstream_analysis/bin/ArchR_preprocessing/ArchR_subsetting.R
+++ NF-downstream_analysis/bin/ArchR_preprocessing/ArchR_subsetting.R
@@ -21,33 +21,18 @@ option_list <- list(
     make_option(c("-r", "--runtype"), action = "store", type = "character", help = "Specify whether running through through 'nextflow' in order to switch paths"),
     make_option(c("-c", "--cores"), action = "store", type = "integer", help = "Number of CPUs"),
     make_option(c("-m", "--meta_col1"), action = "store", type = "character", help = "Name of first metadata column containing groups to subset", default = NULL),
-    make_option(c("", "--meta_col2"), action = "store", type = "character", help = "Name of second metadata column containing groups to subset", default = NULL),
-    make_option(c("-o", "--output"), action = "store", type = "character", help = "Name of output RDS file", default = 'seurat_subset'),
+    #make_option(c("", "--meta_col2"), action = "store", type = "character", help = "Name of second metadata column containing groups to subset", default = NULL),
+    #make_option(c("-o", "--output"), action = "store", type = "character", help = "Name of output ArchR project", default = 'ArchR_subset'),
     make_option(c("-g", "--groups1"), action = "store", type = "character", help = "Classifications of cells (within meta_col1) to subset from dataset. \
     If multiple classifications are used to subest, must be provided as a comma separated list i.e. --groups celltype1,celltype2", default = NULL),
-    make_option(c("", "--groups2"), action = "store", type = "character", help = "Classifications of cells (within meta_col2) to subset from dataset.", default = NULL),
-    make_option(c("-i", "--invert1"), action = "store", type = "logical", help = "Boolean for whether to invert groups1 selection", default = FALSE),
-    make_option(c("", "--invert2"), action = "store", type = "logical", help = "Boolean for whether to invert groups2 selection", default = FALSE),
+    #make_option(c("", "--groups2"), action = "store", type = "character", help = "Classifications of cells (within meta_col2) to subset from dataset.", default = NULL),
+    #make_option(c("-i", "--invert1"), action = "store", type = "logical", help = "Boolean for whether to invert groups1 selection", default = FALSE),
+    #make_option(c("", "--invert2"), action = "store", type = "logical", help = "Boolean for whether to invert groups2 selection", default = FALSE),
     make_option(c("", "--verbose"), action = "store_true", type = "logical", help = "Verbose", default = FALSE))
 
 opt_parser = OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 if(opt$verbose) print(opt)
-
-if(!is.null(opt$groups1)){
-    opt$groups1 = strsplit(opt$groups1, ',')[[1]]
-}
-if(!is.null(opt$groups2)){
-opt$groups2 = strsplit(opt$groups2, ',')[[1]]
-}
-
-if(is.na(opt$meta_col1)){
-    stop("meta_col1 parameter must be provided. See script usage (--help)")
-}
-
-if(is.null(opt$groups1)){
-    stop("groups1 parameter must be provided. See script usage (--help)")
-}
 
 # Set paths and load data
 {
@@ -56,11 +41,19 @@ if(is.null(opt$groups1)){
     
     ncores = 8
     
-    plot_path = "../output/NF-downstream_analysis/4_ArchR_filter_clusters/plots/"
-    rds_path = "../output/NF-downstream_analysis/4_ArchR_filter_clusters/rds_files/"
-    data_path = "../output/NF-downstream_analysis/3_ArchR_clustering_prefiltering/rds_files/"
+    #plot_path = "./output/NF-downstream_analysis/4_ArchR_filter_clusters/plots/"
+    #rds_path = "./output/NF-downstream_analysis/4_ArchR_filter_clusters/rds_files/"
+    data_path = "./output/NF-downstream_analysis/ArchR_preprocessing/ss8/ArchR_integration/rds_files/"
 
-    addArchRThreads(threads = 1) 
+    addArchRThreads(threads = 1)
+    
+    opt$invert1 = FALSE
+    opt$invert2 = FALSE
+    opt$groups1 = NULL
+    opt$groups2 = NULL
+    opt$meta_col1 = NULL
+    opt$meta_col2 = NULL
+  
     
   } else if (opt$runtype == "nextflow"){
     cat('pipeline running through Nextflow\n')
@@ -81,6 +74,25 @@ if(is.null(opt$groups1)){
   dir.create(rds_path, recursive = T)
 }
 
+# Set up options
+if(!is.null(opt$groups1)){
+  opt$groups1 = strsplit(opt$groups1, ',')[[1]]}
+#if(!is.null(opt$groups2)){
+#  opt$groups2 = strsplit(opt$groups2, ',')[[1]]}
+
+if(is.na(opt$meta_col1)){
+  stop("meta_col1 parameter must be provided. See script usage (--help)")}
+if(is.null(opt$groups1)){
+  stop("groups1 parameter must be provided. See script usage (--help)")}
+
+
+############################## Function to subset ArchR project #######################################
+subset_ArchR <- function(ArchR_object, meta_col, groups){
+  idxPass <- which(ArchR_object@cellColData[,opt$meta_col] %in% opt$groups)
+  cellsPass <- ArchR$cellNames[idxPass]
+  ArchR_filtered <- ArchR[cellsPass, ]
+  return(ArchR_filtered)
+}
 
 ############################## Read in ArchR project #######################################
 # Retrieve object label
@@ -91,49 +103,12 @@ print(label)
 ArchR <- loadArchRProject(path = paste0(data_path, label, "_Save-ArchR"), force = FALSE, showLogo = TRUE)
 paste0("Memory Size = ", round(object.size(ArchR) / 10^6, 3), " MB")
 
-############################## Subset ArchR object #######################################
-# If invert is true, then subset the inverted groups from the seurat object
-if(opt$invert1 == TRUE){
-    opt$groups1 <- as.character(unique(seurat_data@meta.data[[opt$meta_col1]])[!unique(seurat_data@meta.data[[opt$meta_col1]]) %in% opt$groups1])
-}
-if(opt$invert2 == TRUE){
-    opt$groups2 <- as.character(unique(seurat_data@meta.data[[opt$meta_col2]])[!unique(seurat_data@meta.data[[opt$meta_col2]]) %in% opt$groups2])
-}
+# see all available metadata cols which could be used to subset
+print(colnames(ArchR@cellColData))
 
-if(!is.null(opt$meta_col2)){
-    seurat_subset <- subset(seurat_data, subset = !!as.symbol(opt$meta_col1) %in% opt$groups1 & !!as.symbol(opt$meta_col2) %in% opt$groups2)
-    seurat_subset@meta.data[c(opt$meta_col1, opt$meta_col2)] <- lapply(seurat_subset@meta.data[c(opt$meta_col1, opt$meta_col2)], droplevels)
-}else{
-    seurat_subset <- subset(seurat_data, subset = !!as.symbol(opt$meta_col1) %in% opt$groups1)
-    seurat_subset@meta.data[opt$meta_col1] <- lapply(seurat_subset@meta.data[opt$meta_col1], droplevels)
-}
+############################## Subset ArchR object #######################################
+### will need to add extra functionality here 
+ArchR_subset <- subset_ArchR(ArchR, meta_col = opt$meta_col1, groups = opt$groups1)
 
 ############################## Save new ArchR object #######################################
-saveArchRProject(ArchRProj = ArchR, outputDirectory = paste0(rds_path, label, "_Save-ArchR"), load = FALSE)
-
-############################## Plots #######################################
-seurat_data@meta.data[c(opt$meta_col1, opt$meta_col2)] <- lapply(seurat_data@meta.data[c(opt$meta_col1, opt$meta_col2)], as.factor)
-
-colours = ggPlotColours(length(levels(seurat_data@meta.data[[opt$meta_col1]])))
-
-max_x = round(max(seurat_data@reductions$umap@cell.embeddings[,1]))
-min_x = round(min(seurat_data@reductions$umap@cell.embeddings[,1]))
-max_y = round(max(seurat_data@reductions$umap@cell.embeddings[,2]))
-min_y = round(min(seurat_data@reductions$umap@cell.embeddings[,2]))
-
-full_plot = DimPlot(seurat_data, group.by = opt$meta_col1, pt.size = 0.3, cols = colours, label = TRUE, label.size = 3, label.box = TRUE) +
-                ylim(min_y, max_y) +
-                xlim(min_x, max_x) +
-                theme(legend.position = "none") +
-                ggtitle("")
-
-subset_plot = DimPlot(seurat_data, group.by = opt$meta_col1, pt.size = 0.3, cols = colours[levels(seurat_data@meta.data[[opt$meta_col1]]) %in% opt$groups1],
-        cells = rownames(seurat_data@meta.data)[seurat_data@meta.data[[opt$meta_col1]] %in% opt$groups1]) +
-                ylim(min_y, max_y) +
-                xlim(min_x, max_x) +
-                theme(legend.position = "none") +
-                ggtitle("")
-
-png(paste0(plot_path, 'cell_subset_umap.png'), width = 40, height = 20, units = 'cm', res = 200)
-grid.arrange(full_plot, subset_plot, nrow = 1)
-graphics.off()
+saveArchRProject(ArchRProj = ArchR_subset, outputDirectory = paste0(rds_path, label, "_Save-ArchR"), load = FALSE)
