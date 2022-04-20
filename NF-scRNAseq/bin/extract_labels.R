202a98bcf76d0f893259fdc66acc0d8115396aac
Eva <ehamrud@gmail.com>
2022-04-20 11:38:08 +0100

    remove NF-scRNA pipeline as this is integrated in 10x repo

--- NF-scRNAseq/bin/extract_labels.R
+++
@@ -1,7 +0,0 @@
-### will need to read in transfer_labels object
-
-sc_Helper_cell_states_df <- data.frame(cell_ids = colnames(seurat_data),
-                                       scHelper_cell_type = seurat_data@meta.data$scHelper_cell_type)
-
-
-write.csv(sc_Helper_cell_states_df, "./TEST.csv")
--- NF-scRNAseq/run_scripts/local_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nextflow run ./main.nf \
---outdir ../output/NF-scRNAseq \
--profile local_test \
--resume
