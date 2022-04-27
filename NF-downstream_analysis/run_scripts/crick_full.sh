1834c01f96ddf8372e94f1d74f60fad15a0afba5
Eva <ehamrud@gmail.com>
2022-04-27 16:41:25 +0100

    remove dump hashes

--- NF-downstream_analysis/run_scripts/crick_full.sh
+++ NF-downstream_analysis/run_scripts/crick_full.sh
@@ -14,7 +14,8 @@ ml Graphviz
 
 export NXF_VER=21.10.3
 
-nextflow run ./main.nf -dump-hashes \
+##nextflow run ./main.nf -dump-hashes \
+nextflow run ./main.nf \
 --outdir ../output/NF-downstream_analysis \
 -profile crick_full \
 -resume
