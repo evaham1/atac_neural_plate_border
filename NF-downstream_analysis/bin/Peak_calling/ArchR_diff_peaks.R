249d6368bce4af4aeb473d0329050f6eb8bdc8cc
Eva <ehamrud@gmail.com>
2022-05-10 11:44:23 +0100

    updating distal/proximal peak heatmaps

--- NF-downstream_analysis/bin/Peak_calling/ArchR_diff_peaks.R
+++ NF-downstream_analysis/bin/Peak_calling/ArchR_diff_peaks.R
@@ -42,6 +42,9 @@ if(opt$verbose) print(opt)
     #plot_path = "./output/NF-downstream_analysis/ArchR_peak_calling/ss8/2_differential_peaks/plots/"
     data_path = "./output/NF-downstream_analysis/ArchR_preprocessing/QC_HIGH/HH6/prefiltering/peak_calling/rds_files/"
     
+    #ss8
+    data_path = "./output/NF-downstream_analysis/ArchR_preprocessing/FILTERING/ss8/postfiltering/peak_calling/rds_files/"
+    
     addArchRThreads(threads = 1) 
     
   } else if (opt$runtype == "nextflow"){
@@ -64,10 +67,10 @@ if(opt$verbose) print(opt)
 }
 
 ############################### FUNCTIONS ####################################
-### function to extract top n logFC for each group from summarized experiment object
+### function to extract top n logFC for each group from summarized experiment object (no filter on FDR!)
 # returns a summarised experiment object with only the top n features per cell group
-extract_top_features <- function(markers, n = 10) {
-  markerList <- getMarkers(markers, cutOff = "FDR <= 1")
+extract_top_features <- function(markersPeaks, n = 10) {
+  markerList <- getMarkers(markersPeaks, cutOff = "FDR <= 1 & Log2FC >= 0") # only keep upregulated peaks
   df <- data.frame()
   for (i in 1:length(names(markerList))) {
     print(i)
@@ -78,11 +81,23 @@ extract_top_features <- function(markers, n = 10) {
     group_by(group_name) %>%
     top_n(n, Log2FC) %>%
     dplyr::arrange(Log2FC, .by_group = TRUE)
-  top_markers <- df$idx # top 15 markers by logF2C between cell groups
-  top_markers <- unique(top_markers) # some of these markers are shared??
-  coords <-  rownames(markers)[rownames(markers) %in% top_markers]
-  top_markers_se <- markers[coords, ]
+  top_markers <- df$idx
+  top_markers <- unique(top_markers)
+  coords <-  rownames(markersPeaks)[rownames(markersPeaks) %in% top_markers]
+  top_markers_se <- markersPeaks[coords, ]
   return(top_markers_se)
+}
+
+### function to turn summarised experiment object into a df that can be printed to see diff features
+extract_features_table <- function(markersPeaks) {
+  markerList <- getMarkers(markersPeaks, cutOff = "FDR <= 1") # keep all peaks
+  df <- data.frame()
+  for (i in 1:length(names(markerList))) {
+    print(i)
+    df_i <- as.data.frame(markerList[i])
+    df <- rbind(df, df_i)
+  }
+  return(df)
 }
 
 ############################## Read in ArchR project #######################################
@@ -111,13 +126,12 @@ getAvailableMatrices(ArchR)
 # idxSample <- BiocGenerics::which(ArchR_big$scHelper_cell_type_old %in% c("NC", "dNC", "aPPR", "pPPR"))
 # cellsSample <- ArchR_big$cellNames[idxSample]
 # 
-# ArchR <- subsetArchRProject(ArchR_big, cellsSample, outputDirectory = "ArchRSubset",
-#   dropCells = TRUE, logFile = NULL, force = TRUE)
+# ArchR <- subsetArchRProject(ArchR_big, cellsSample, outputDirectory = "ArchRSubset", dropCells = TRUE, logFile = NULL, force = TRUE)
 # 
 # ArchR <- loadArchRProject(path = "./ArchRSubset", force = TRUE, showLogo = TRUE)
 
 
-############################# CLUSTER Marker Peaks #######################################
+############################# Calculate Diff Peaks between clusters #######################################
 
 markersPeaks <- getMarkerFeatures(
   ArchRProj = ArchR, 
@@ -125,63 +139,126 @@ markersPeaks <- getMarkerFeatures(
   groupBy = "clusters")
 markersPeaks
 
+peaks_table <- extract_features_table(markersPeaks)
+write.csv(peaks_table, paste0(plot_path, "all_differentially_expressed_peaks.csv"), row.names = FALSE)
+
+############################# Heatmap of peaks that pass threshold #######################################
+
 heatmapPeaks <- plotMarkerHeatmap(
   seMarker = markersPeaks, 
   cutOff = "FDR <= 0.3 & Log2FC >= 0.5",
   nLabel = 3)
-
-png(paste0(plot_path, 'clusters_diff_peak_cutoff_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
+png(paste0(plot_path, 'diff_peak_cutoff_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
 draw(heatmapPeaks, heatmap_legend_side = "bot", annotation_legend_side = "bot")
 graphics.off()
 
-# https://github.com/maehrlab/pharyngeal_endoderm_development/blob/main/scATAC_qc_analysis/scATAC_analysis/scATAC_10_Fig3a_3b.ipynb
-add_name = function(X, c) {
-  if(nrow(X)==0) return(NULL)
-  X$cluster = c
-  X
-}
-## All peaks:
-marker_tables = markersPeaks %>% getMarkers(cutOff = "FDR <= 1 & Log2FC >= 0.1")
-marker_tables_S = mapply(add_name, marker_tables, names(marker_tables), SIMPLIFY = F) %>% Reduce(f = rbind)
-mixedrank = function(x) order(gtools::mixedorder(x))
-markers_top_S = marker_tables_S %>%
-  as.data.frame() %>%
-  #dplyr::mutate( highest_logfc_by_gene = ave(Log2FC, name, FUN = max)) %>%
-  #dplyr::mutate( is_highest = Log2FC == highest_logfc_by_gene) %>%
-  #subset(is_highest) %>%
-  dplyr::group_by(cluster) %>%
-  dplyr::mutate(rank = rank(-Log2FC, ties = "first")) %>%
-  # dplyr::top_n(Log2FC, n = 1000000) %>%
-  dplyr::arrange(mixedrank(cluster), desc(Log2FC))
-write.csv(markers_top_S, paste0(plot_path, "all_differentially_expressed_peaks.csv"), row.names = FALSE)
+############################# Heatmap of top 50 peaks per cell group #######################################
 
-# Plot top peaks:
-markers_top = marker_tables_S %>%  
-  as.data.frame() %>%
-  # dplyr::mutate( highest_logfc_by_gene = ave(Log2FC, name, FUN = max)) %>%
-  # dplyr::mutate( is_highest = Log2FC == highest_logfc_by_gene) %>%
-  # subset(is_highest) %>%
-  dplyr::group_by(cluster) %>%
-  dplyr::mutate(rank = rank(-Log2FC, ties = "first")) %>%
-  dplyr::top_n(Log2FC, n = 50) %>%
-  dplyr::arrange(mixedrank(cluster), desc(Log2FC))
-#markers_label   = markers_top %>% subset(rank<=2,  select = "name", drop = T) 
-#markers_include = markers_top %>% subset(rank<=4000000, select = "unique_id", drop = T) 
-markers_include <- markers_top$idx
-#length(markers_include)
-marker_subset = markersPeaks[rowData(markersPeaks)$idx %in% markers_include,]
+subset_peaks <- extract_top_features(markersPeaks, n = 50)
 
 heatmapGS <- plotMarkerHeatmap(
   seMarker = marker_subset, 
   cutOff = "FDR <= 1 & Log2FC >= 0.1",
-  #labelMarkers = labelMarkers,
   pal = viridisLite::mako(256),
-  nLabel = 3
-)
-
-png(paste0(plot_path, 'clusters_diff_peak_top50_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
+  nLabel = 3)
+png(paste0(plot_path, 'diff_peak_top50_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
 print(heatmapGS)
 graphics.off()
+
+
+############################# Add peak information to markersPeaks object #######################################
+
+tmp_peaks = data.frame(ArchR@peakSet)
+tmp_diff_peaks = data.frame(rowData(markersPeaks))
+
+diff_peaks_join_peakset = left_join(tmp_diff_peaks, tmp_peaks, by = c("seqnames" = "seqnames", "start" = "start", "end" = "end"))
+diff_peaks_join_peakset$name = paste(diff_peaks_join_peakset$nearestGene, diff_peaks_join_peakset$distToTSS,sep="_")
+diff_peaks_join_peakset$unique_id = paste(diff_peaks_join_peakset$seqnames, diff_peaks_join_peakset$start, diff_peaks_join_peakset$end, sep=":")
+rowData(markersPeaks) = diff_peaks_join_peakset
+
+
+############################# Promoter Peaks #######################################
+
+markersPeaks_promoter <- subset(markersPeaks, rowData(markersPeaks)$peakType == "Promoter")
+marker_tables_promoter = markersPeaks_promoter %>% getMarkers(cutOff = "FDR <= 0.1 & Log2FC >= 0.5")
+marker_tables_promoter_tmp = marker_tables_promoter %>%  as.data.frame() 
+write.csv(marker_tables_promoter_tmp, paste0(plot_path, "all_differentially_expressed_peaks_promoter.csv"), row.names = FALSE)
+
+heatmapPeaks <- plotMarkerHeatmap(
+  seMarker = markersPeaks_promoter, 
+  cutOff = "FDR <= 0.3 & Log2FC >= 0.5",
+  nLabel = 3)
+png(paste0(plot_path, 'diff_promoter_peak_cutoff_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
+draw(heatmapPeaks, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+graphics.off()
+
+############################# Distal Peaks #######################################
+
+markersPeaks_distal <- subset(markersPeaks, rowData(markersPeaks)$peakType == "Distal")
+marker_tables_distal = markersPeaks_distal %>% getMarkers(cutOff = "FDR <= 0.1 & Log2FC >= 0.5")
+marker_tables_distal_tmp = marker_tables_distal %>%  as.data.frame() 
+write.csv(marker_tables_distal_tmp, paste0(plot_path, "all_differentially_expressed_peaks_distal.csv"), row.names = FALSE)
+
+heatmapPeaks <- plotMarkerHeatmap(
+  seMarker = markersPeaks_distal, 
+  cutOff = "FDR <= 0.3 & Log2FC >= 0.5",
+  nLabel = 3)
+png(paste0(plot_path, 'diff_distal_peak_cutoff_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
+draw(heatmapPeaks, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+graphics.off()
+
+
+################################################################
+###################   OLD STUFF   #############################
+
+# # https://github.com/maehrlab/pharyngeal_endoderm_development/blob/main/scATAC_qc_analysis/scATAC_analysis/scATAC_10_Fig3a_3b.ipynb
+# add_name = function(X, c) {
+#   if(nrow(X)==0) return(NULL)
+#   X$cluster = c
+#   X
+# }
+# ## All peaks:
+# marker_tables = markersPeaks %>% getMarkers(cutOff = "FDR <= 1 & Log2FC >= 0.1")
+# marker_tables_S = mapply(add_name, marker_tables, names(marker_tables), SIMPLIFY = F) %>% Reduce(f = rbind)
+# mixedrank = function(x) order(gtools::mixedorder(x))
+# markers_top_S = marker_tables_S %>%
+#   as.data.frame() %>%
+#   #dplyr::mutate( highest_logfc_by_gene = ave(Log2FC, name, FUN = max)) %>%
+#   #dplyr::mutate( is_highest = Log2FC == highest_logfc_by_gene) %>%
+#   #subset(is_highest) %>%
+#   dplyr::group_by(cluster) %>%
+#   dplyr::mutate(rank = rank(-Log2FC, ties = "first")) %>%
+#   # dplyr::top_n(Log2FC, n = 1000000) %>%
+#   dplyr::arrange(mixedrank(cluster), desc(Log2FC))
+# write.csv(markers_top_S, paste0(plot_path, "all_differentially_expressed_peaks.csv"), row.names = FALSE)
+# 
+# # Plot top peaks:
+# markers_top = marker_tables_S %>%  
+#   as.data.frame() %>%
+#   # dplyr::mutate( highest_logfc_by_gene = ave(Log2FC, name, FUN = max)) %>%
+#   # dplyr::mutate( is_highest = Log2FC == highest_logfc_by_gene) %>%
+#   # subset(is_highest) %>%
+#   dplyr::group_by(cluster) %>%
+#   dplyr::mutate(rank = rank(-Log2FC, ties = "first")) %>%
+#   dplyr::top_n(Log2FC, n = 50) %>%
+#   dplyr::arrange(mixedrank(cluster), desc(Log2FC))
+# #markers_label   = markers_top %>% subset(rank<=2,  select = "name", drop = T) 
+# #markers_include = markers_top %>% subset(rank<=4000000, select = "unique_id", drop = T) 
+# markers_include <- markers_top$idx
+# #length(markers_include)
+# marker_subset = markersPeaks[rowData(markersPeaks)$idx %in% markers_include,]
+# 
+# heatmapGS <- plotMarkerHeatmap(
+#   seMarker = marker_subset, 
+#   cutOff = "FDR <= 1 & Log2FC >= 0.1",
+#   #labelMarkers = labelMarkers,
+#   pal = viridisLite::mako(256),
+#   nLabel = 3
+# )
+# 
+# png(paste0(plot_path, 'clusters_diff_peak_top50_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
+# print(heatmapGS)
+# graphics.off()
 
 # ######################
 # # subset for proximal/distal/etc
