8d4e2bfa27918c780dde0e9ba0777c81a2a0b5cb
Eva <ehamrud@gmail.com>
2022-12-24 10:16:33 +0000

    cluser identities save archr!

--- NF-downstream_analysis/bin/Integration/ArchR_cluster_identities.R
+++ NF-downstream_analysis/bin/Integration/ArchR_cluster_identities.R
@@ -340,9 +340,14 @@ graphics.off()
 
 new_labels <- cluster_idents[,2]
 names(new_labels) <- cluster_idents[,1]
-ArchR$cluster_old_labels <- mapLabels(ArchR$clusters, newLabels = new_labels)
+ArchR$cluster_labels <- mapLabels(ArchR$clusters, newLabels = new_labels)
 
-p1 <- plotEmbedding(ArchR, name = "cluster_old_labels", plotAs = "points", size = 1.8, baseSize = 0, 
+# save ArchR object with cluster labels
+paste0("Memory Size = ", round(object.size(ArchR) / 10^6, 3), " MB")
+saveArchRProject(ArchRProj = ArchR, outputDirectory = paste0(rds_path, label[1], "_Save-ArchR"), load = FALSE)
+
+# plot cluster labels on UMAPs
+p1 <- plotEmbedding(ArchR, name = "cluster_labels", plotAs = "points", size = 1.8, baseSize = 0, 
               labelSize = 8, legendSize = 0, labelAsFactors = FALSE)
 p2 <- plotEmbedding(ArchR, name = "scHelper_cell_type_old", plotAs = "points", size = 1.8, baseSize = 0, 
               labelSize = 8, legendSize = 0, pal = atac_scHelper_old_cols, labelAsFactors = FALSE)
@@ -354,57 +359,3 @@ graphics.off()
 png(paste0(plot_path, 'assigned_cluster_idents_UMAP_comparison.png'), height = 20, width = 40, units = 'cm', res = 400)
 print(p1 + p2)
 graphics.off()
-
-##################### Distribution of labels across stages ##################################
-
-if (length(unique(ArchR$stage)) > 1){
-
-  plot_path = "./plots/labels_by_stage_distribution/"
-  dir.create(plot_path, recursive = T)
-  
-  png(paste0(plot_path, 'counts_by_stage_table.png'), height = 25, width = 40, units = 'cm', res = 400)
-  cell_counting(ArchR = ArchR, group1 = "scHelper_cell_type_old", group2 = "stage", scHelper_cell_type_order = scHelper_cell_type_order)
-  graphics.off()
-  
-  # visualise distribution across stages: confusion matrix
-  png(paste0(plot_path, "stage_distribution.png"), width=25, height=20, units = 'cm', res = 200)
-  cell_counts_heatmap(ArchR = ArchR, group1 = "scHelper_cell_type_old", group2 = "stage")
-  graphics.off()
-  
-  # visualise distribution across stages: piecharts
-  counts <- cell_counting(ArchR = ArchR, group1 = "scHelper_cell_type_old", group2 = "stage", print_table = FALSE, scHelper_cell_type_order = scHelper_cell_type_order)
-  png(paste0(plot_path, "label_by_stage_piecharts_unscaled.png"), width=50, height=40, units = 'cm', res = 200)
-  cell_counts_piecharts(counts, col = scHelper_cell_type_colours)
-  graphics.off()
-  
-  png(paste0(plot_path, "label_by_stage_piecharts_scaled.png"), width=50, height=40, units = 'cm', res = 200)
-  cell_counts_piecharts(counts, col = scHelper_cell_type_colours, scale = TRUE)
-  graphics.off()
-  
-##################### Distribution of stages across labels ##################################
-  
-  # visualise distribution across stages: piecharts
-  counts <- cell_counting(ArchR = ArchR, group1 = "stage", group2 = "scHelper_cell_type_old", print_table = FALSE, scHelper_cell_type_order = scHelper_cell_type_order)
-  png(paste0(plot_path, "stage_by_label_piecharts_unscaled.png"), width=50, height=40, units = 'cm', res = 200)
-  cell_counts_piecharts(counts, col = stage_colours)
-  graphics.off()
-  
-  png(paste0(plot_path, "stage_by_label_piecharts_scaled.png"), width=50, height=40, units = 'cm', res = 200)
-  cell_counts_piecharts(counts, col = stage_colours, scale = TRUE)
-  graphics.off()
-  
-##################### Distribution of rna stages across atac stages ##################################
-
-  rna_stages <- plotEmbedding(ArchR, name = "rna_stage", plotAs = "points", size = 1.8, baseSize = 0, 
-                            labelSize = 8, legendSize = 0, labelAsFactors = FALSE, pal = stage_colours)
-  atac_stages <- plotEmbedding(ArchR, name = "stage", plotAs = "points", size = 1.8, baseSize = 0, 
-                             labelSize = 8, legendSize = 0, labelAsFactors = FALSE, pal = stage_colours)
-  png(paste0(plot_path, 'UMAPs_rna_stages_VS_atac_stages.png'), height = 20, width = 40, units = 'cm', res = 400)
-  print(rna_stages + atac_stages)
-  graphics.off()
-
-  png(paste0(plot_path, "rna_atac_stage_distribution.png"), width=25, height=20, units = 'cm', res = 200)
-  cell_counts_heatmap(ArchR = ArchR, group1 = "rna_stage", group2 = "stage")
-  graphics.off()
-
-}
