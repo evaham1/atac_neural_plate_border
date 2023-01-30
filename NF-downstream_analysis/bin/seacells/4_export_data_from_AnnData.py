4dd678a673d3f457367cda9c211ed35c95c36fdc
Eva <ehamrud@gmail.com>
2023-01-30 12:27:29 +0000

    finish script to export anndata info

--- NF-downstream_analysis/bin/seacells/4_export_data_from_AnnData.py
+++ NF-downstream_analysis/bin/seacells/4_export_data_from_AnnData.py
@@ -35,15 +35,16 @@ def main(args=None):
 
     #####   1) export for the full anndata object, need cell_ids:seacell_ids dictionary
     # Load data
-    ad_full = sc.read('AnnData_metacells_assigned.h5ad')
+    ad_full = sc.read(args.input + '/AnnData_metacells_assigned.h5ad')
     print(ad_full)
     # Export data
-    ad.obs.to_csv('AnnData_metacells_assigned_cell_metadata.csv')
+    ad_full.obs.to_csv(os.path.join(rds_path,'AnnData_metacells_assigned_cell_metadata.csv'))
 
     #####   2) export for the summarised anndata object, need summarised peak count matrix
-    ad_sum = sc.read('AnnData_summarised_by_metacells.h5ad')
+    ad_sum = sc.read(args.input + '/AnnData_summarised_by_metacells.h5ad')
     print(ad_sum)
     # Export data
+    ad_sum.to_df(layer="raw").to_csv(os.path.join(rds_path,"AnnData_summarised_by_metacells_peak_counts.csv"))
 
 if __name__ == '__main__':
     sys.exit(main())
