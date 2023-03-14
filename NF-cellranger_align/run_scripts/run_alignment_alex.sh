2ee5de7d1d974f39165e76e2d3fdaaf9abdfc586
Alex Thiery <alexthiery1@gmail.com>
2023-03-14 16:34:31 +0000

    test atac alignment

---
+++ NF-cellranger_align/run_scripts/run_alignment_alex.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#SBATCH --job-name=NF-cellranger_atac_align
+#SBATCH -t 72:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=thierya@crick.ac.uk
+
+export TERM=xterm
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/22.10.1
+ml Singularity/3.4.2
+ml Graphviz
+
+export NXF_VER=22.10.1
+export NXF_SINGULARITY_CACHEDIR=/camp/home/thierya/working/singularity
+
+## UPDATE PIPLINE
+nextflow pull Streit-lab/cellranger_multiomic
+
+nextflow run Streit-lab/cellranger_multiomic \
+    -r test_release \
+    -c ./conf/crick_params.config \
+    --sample_sheet ./samplesheet.csv \
+    --outdir ../output/NF-cellranger_align \
+    --email eva.hamrud@crick.ac.uk \
+    -resume
