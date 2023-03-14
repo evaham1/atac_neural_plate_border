b635b6d3c0ca11e98cbe2785657f59a7ec5bdbc3
Eva <ehamrud@gmail.com>
2023-03-14 16:35:40 +0000

    old nf version

--- NF-cellranger_align/run_scripts/run_alignment.sh
+++ NF-cellranger_align/run_scripts/run_alignment.sh
@@ -6,12 +6,12 @@
 
 ## LOAD REQUIRED MODULES
 ml purge
-ml Nextflow/22.10.3
+ml Nextflow/21.10.6
 ml Singularity/3.6.4
 ml Graphviz
 
 export TERM=xterm
-export NXF_VER=22.10.3
+export NXF_VER=21.10.6
 export NXF_SINGULARITY_CACHEDIR=/nemo/lab/briscoej/working/hamrude/NF_singularity
 export NXF_HOME=/nemo/lab/briscoej/working/hamrude/nextflow
 export NXF_WORK=work/
