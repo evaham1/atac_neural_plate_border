0d8eb815f051a489cb9405fcba6af0b74a6416d5
Alex Thiery <alexthiery1@gmail.com>
2023-03-14 14:08:55 +0000

    init fetchngs for hichip data

---
+++ NF-hichip/run_scripts/fetchngs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#SBATCH --job-name=NF-hichip_fetchngs
+#SBATCH -t 6:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=thierya@crick.ac.uk
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/22.10.3
+ml Singularity/3.6.4
+ml Graphviz
+
+export TERM=xterm
+export NXF_VER=22.10.3
+export NXF_SINGULARITY_CACHEDIR=/nemo/lab/briscoej/working/thierya/singularity
+export NXF_HOME=/nemo/project/home/thierya/working/nextflow
+
+## UPDATE PIPLINE
+nextflow pull nf-core/fetchngs
+
+nextflow run nf-core/fetchngs \
+    -c ./conf/crick_params.config \
+    --input ids.csv \
+    --outdir ../output/NF-hichip_fetchngs \
+    --email thierya@crick.ac.uk \
+    -resume
