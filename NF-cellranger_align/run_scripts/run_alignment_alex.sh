51d3488fbddac2d80327185ce1d1f65356059fd5
Alex Thiery <alexthiery1@gmail.com>
2023-03-15 17:02:23 +0000

    reset

--- NF-cellranger_align/run_scripts/run_alignment_alex.sh
+++ NF-cellranger_align/run_scripts/run_alignment_alex.sh
@@ -13,16 +13,15 @@ ml Singularity/3.4.2
 ml Graphviz
 
 export NXF_VER=21.10.6
-export NXF_SINGULARITY_CACHEDIR=/nemo/lab/briscoej/working/thierya/singularity
-export NXF_HOME=/nemo/lab/briscoej/working/thierya/nextflow
-export NXF_WORK=work/
+export NXF_SINGULARITY_CACHEDIR=/camp/home/thierya/working/singularity
+export NXF_HOME=/nemo/project/home/thierya/.nextflow
 
 ## UPDATE PIPLINE
 nextflow pull Streit-lab/cellranger_multiomic
 
 nextflow run Streit-lab/cellranger_multiomic \
     -r main \
-    -c ./conf/crick_params_alex.config \
+    -c ../conf/crick_params_alex.config \
     --sample_sheet ./samplesheet_alex.csv \
     --outdir ../output/NF-cellranger_align \
     --email thierya@crick.ac.uk \
