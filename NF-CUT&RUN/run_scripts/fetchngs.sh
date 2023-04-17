dab38cbcebe1ab4ca6cf329cce64abb5db1312cd
Eva <ehamrud@gmail.com>
2023-04-17 17:07:10 +0100

    set up data fetch H3k27ac cutandrun

---
+++ NF-CUT&RUN/run_scripts/fetchngs.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#SBATCH --job-name=NF-cutandrun_fetchngs
+#SBATCH -t 6:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=hamrude@crick.ac.uk
+
+export TERM=xterm
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Java/11.0.2
+ml Nextflow/22.10.3
+ml Singularity/3.6.4
+ml Graphviz
+
+export NXF_VER=22.10.3
+
+## UPDATE PIPLINE
+nextflow pull nf-core/fetchngs
+
+nextflow run nf-core/fetchngs \
+    -r 1.9 \
+    -c ./conf/crick_params.config \
+    --input ./data/SRR_Acc_List.txt \
+    --outdir ../output/NF-cutandrun-H3k27ac \
+    --nf_core_pipeline cutandrun \
+    --email hamrude@crick.ac.uk \
+    -resume
