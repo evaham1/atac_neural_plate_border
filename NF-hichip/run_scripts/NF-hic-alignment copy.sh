0261011e544bb62700ad2a587b541f1ba73ff988
Eva <ehamrud@gmail.com>
2023-04-18 15:42:05 +0100

    fithichip runscript

---
+++ NF-hichip/run_scripts/NF-hic-alignment copy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name=NF-FitHiChiP
+#SBATCH -t 42:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=hamrude@crick.ac.uk
+
+export TERM=xterm
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Java/11.0.2
+ml Singularity/3.6.4
+
+## RUN FITHICHIP BASH SCRIPT
+bash ../FitHiChIP/FitHiChIP_Singularity.sh -C ../FitHiChIP/data/configfile_NF_r1
