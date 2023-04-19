bc120e1dbc4d8a411f9835eaa3c2af2dca3e08ff
Eva <ehamrud@gmail.com>
2023-04-19 12:39:05 +0100

    update to run on pulled sing image

--- NF-hichip/run_fithichip/FitHiChIP_Singularity_temp.sh
+++ NF-hichip/run_fithichip/FitHiChIP_Singularity_temp.sh
@@ -796,5 +796,5 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # singularity exec -B "$BINDARG" library://tuvan/default/fithichip bash "/FitHiChIP/FitHiChIP_HiCPro.sh" -C "$ConfigFile" -s
-singularity exec -B "$BINDARG" aylab/fithichip bash "/FitHiChIP/FitHiChIP_HiCPro.sh" -C "$ConfigFile" -s
+singularity exec -B "$BINDARG" /nemo/lab/briscoej/working/hamrude/singularity/fithichip.sif bash "/FitHiChIP/FitHiChIP_HiCPro.sh" -C "$ConfigFile" -s
 
