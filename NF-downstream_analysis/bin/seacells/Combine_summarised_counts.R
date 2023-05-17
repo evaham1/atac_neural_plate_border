a87f1f3fd5417b9950ac16a0fb5594e38ae65a4b
Eva <ehamrud@gmail.com>
2023-05-17 14:54:58 +0100

    fix combining counts

--- NF-downstream_analysis/bin/seacells/Combine_summarised_counts.R
+++ NF-downstream_analysis/bin/seacells/Combine_summarised_counts.R
@@ -32,7 +32,9 @@ if(opt$verbose) print(opt)
     ncores = 8
     
     # interactive paths
-    data_path = "./output/temp_renamed_seacell_outputs//"
+    data_path = "./output/temp_renamed_seacell_outputs/" # made this folder and copied across inputs manually
+    data_path = "./output/NF-downstream_analysis/Processing/ss8/SEACELLS_ATAC_WF/2_SEACells_computed_renamed/csv_files/" # just for renamed ATAC SEACell outputs
+    data_path = "./output/NF-downstream_analysis/Processing/ss8/Integrated_SEACells_label_transfer/rds_files/" # just for the integrated outputs
     rds_path = "./output/NF-downstream_analysis/Processing/FullData/Combined_SEACell_outputs/"
     
   } else if (opt$runtype == "nextflow"){
@@ -78,10 +80,22 @@ print("Checking feature metadata...")
 feature_metadata_paths <- list.files(path = data_path, pattern = "*_feature_metadata.csv", full.names = TRUE)
 print(paste0("Feature metadata paths: ", feature_metadata_paths))
 
+# Check all feature files are identical
 feature_metadata_files <- lapply(feature_metadata_paths, read.csv)
 ifelse (all_identical(feature_metadata_files), print("All feature metadata feature files identical!"), stop("Not all metadata feature files are identical!"))
 
-write.csv(feature_metadata_files[[1]], paste0(rds_path, 'Feature_metadata.csv'))
+# Extract one feature file and format it
+feature_metadata <- feature_metadata_files[[1]]
+feature_metadata <- feature_metadata[,-1]
+print("Checking if any peak IDs are duplicated:")
+table(duplicated(feature_metadata$X))
+
+# Check
+print("Preview of feature metadata:")
+feature_metadata[1:5, 1:5]
+
+# Save
+write.csv(feature_metadata, paste0(rds_path, 'Feature_metadata.csv'))
 
 print("Feature metadata checked and saved!")
 
@@ -93,18 +107,81 @@ print("Checking cell metadata...")
 cell_metadata_paths <- list.files(path = data_path, pattern = "*_cell_metadata.csv", full.names = TRUE)
 print(paste0("Cell metadata paths: ", cell_metadata_paths))
 
+# Read in 
 cell_metadata_files <- lapply(cell_metadata_paths, read.csv)
 
-## add another column to cell metadata with seacell ID-stage eg SEACell-84-ss8
+# Add another column to cell metadata with seacell ID-stage eg SEACell-84-ss8
 cell_metadata_files <- lapply(cell_metadata_files, function(x) mutate(x, SEACell_stage = paste0(SEACell, "-", stage)))
 
-## combine all cell metacell csvs into one
+# Combine all cell metacell csvs into one
 combined_cell_metacell <- ldply(cell_metadata_files, data.frame)
+combined_cell_metacell <- combined_cell_metacell[,-1]
 
-## write out new csv
+# Check
+print("Preview of cell metadata:")
+combined_cell_metacell[1:5,]
+
+# Save
 write.csv(combined_cell_metacell, paste0(rds_path, 'Combined_cell_metadata.csv'))
 
 print("Cell metadata files combined!")
+
+############################## Combine summarised peak counts #######################################
+
+print("Checking summarised peak counts...")
+
+# Extract paths
+count_paths <- list.files(path = data_path, pattern = "*_summarised_by_metacells_counts.csv", full.names = TRUE)
+print(paste0("Summarised count paths: ", count_paths))
+
+count_files <- lapply(count_paths, fread)
+
+# Extract stages in same order than count files are in (hopefully they don't swap...)
+count_file_names <- sub(".*/", "", count_paths)
+stages <- lapply(count_file_names, function(x) substr(x, 1, 3))
+print(stages)
+
+## Edit seacell ID to be seacell ID-stage eg SEACell-84-ss8
+for (i in 1:length(stages)){
+  stage <- stages[i]
+  data <- count_files[[i]][,-1]
+  
+  print("preview of data before formatting:")
+  print(data[1:4, 1:4])
+  
+  data <- data %>% mutate(SEACell = paste0(V1, "-", stage))
+  data <- data[, -1]
+  data <- data %>% select(SEACell, everything())
+  
+  print("preview of data after formatting:")
+  print(data[1:4, 1:4])
+  
+  assign(paste0(stage, "_data"), data)
+}
+
+# Should all have the same number of columns - can have variable row numbers
+print("Checking dimensions of resulting dfs: ")
+print(dim(HH5_data))
+print(dim(HH6_data))
+print(dim(HH7_data))
+print(dim(ss4_data))
+print(dim(ss8_data))
+
+# Should all have the same number of columns - can have variable row numbers
+print("Checking format of resulting dfs: ")
+print(HH5_data[1:2, 1:2])
+print(HH6_data[1:2, 1:2])
+print(HH7_data[1:2, 1:2])
+print(ss4_data[1:2, 1:2])
+print(ss8_data[1:2, 1:2])
+
+# Combine all into one
+combined_df <- rbindlist(list(HH5_data, HH6_data, HH7_data, ss4_data, ss8_data))
+
+## write out new csv
+write.csv(combined_df, paste0(rds_path, 'Combined_summarised_by_metacells_counts.csv'))
+
+print("Summarised peak counts combined!")
 
 ############################## Combine SEACell integrated metadata files #######################################
 
@@ -134,6 +211,9 @@ for (i in 1:length(SEACell_integrated_metadata_paths)){
     mutate(ATAC = paste0(ATAC, "-", stage)) %>%
     mutate(RNA = paste0(RNA, "-", stage))
   
+  # remove first column
+  file <- file[,-1]
+  
   # add altered df to list of dfs
   SEACell_integrated_metadata_files[[i]] <- file
   
@@ -158,57 +238,3 @@ print(combined_SEACell_metacell[1:5, ])
 write.csv(combined_SEACell_metacell, paste0(rds_path, 'Combined_SEACell_integrated_metadata.csv'))
 
 print("Cell integrated metadata files combined!")
-
-############################## Combine summarised peak counts #######################################
-
-print("Checking summarised peak counts...")
-
-# Extract paths
-count_paths <- list.files(path = data_path, pattern = "*_summarised_by_metacells_counts.csv", full.names = TRUE)
-print(paste0("Summarised count paths: ", count_paths))
-
-count_files <- lapply(count_paths, fread)
-
-# Extract stages in same order than count files are in (hopefully they dont swap...)
-count_file_names <- sub(".*/", "", count_paths)
-stages <- lapply(count_file_names, function(x) substr(x, 1, 3))
-print(stages)
-
-## Edit seacell ID to be seacell ID-stage eg SEACell-84-ss8
-for (i in 1:length(stages)){
-  stage <- stages[i]
-  data <- count_files[[i]]
-  
-  print("preview of data:")
-  print(data[1:4, 1:4])
-  
-  data <- data %>% mutate(SEACell = paste0(V1, "-", stage))
-  data <- data[, -1]
-  data <- data %>% select(SEACell, everything())
-  
-  assign(paste0(stage, "_data"), data)
-}
-
-# Should all have the same number of columns - can have variable row numbers
-print("Checking dimensions of resulting dfs: ")
-print(dim(HH5_data))
-print(dim(HH6_data))
-print(dim(HH7_data))
-print(dim(ss4_data))
-print(dim(ss8_data))
-
-# Should all have the same number of columns - can have variable row numbers
-print("Checking format of resulting dfs: ")
-print(HH5_data[1:2, 1:2])
-print(HH6_data[1:2, 1:2])
-print(HH7_data[1:2, 1:2])
-print(ss4_data[1:2, 1:2])
-print(ss8_data[1:2, 1:2])
-
-# Combine all into one
-combined_df <- rbindlist(list(HH5_data, HH6_data, HH7_data, ss4_data, ss8_data))
-
-## write out new csv
-write.csv(combined_df, paste0(rds_path, 'Combined_summarised_by_metacells_counts.csv'))
-
-print("Summarised peak counts combined!")
