bbc7bb1ec7203b050892c8339b03f5fe27e9f8dc
Eva <ehamrud@gmail.com>
2023-05-25 09:19:37 +0100

    try forcing all numbered chroms

--- NF-hichip-downstream/bin/loop_calling_hicdc.R
+++ NF-hichip-downstream/bin/loop_calling_hicdc.R
@@ -59,7 +59,10 @@ if(opt$verbose) print(opt)
 
     #### TEMP TO DEBUG:
     ncores = 8
-    chrs = NULL
+    chrs = c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
+                "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
+                "chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28", "chr29", "chr30",
+                "chr31", "chr32", "chr33")
     opt$binsize = 5000
     opt$Dmin = 10000
     opt$Dmax = 1000000
