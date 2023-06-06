469074b0ed05f0fa20c5e59cb9282909590de4aa
Eva <ehamrud@gmail.com>
2023-06-06 15:26:39 +0100

    check chr29

--- NF-hichip-downstream/bin/loop_calling_hicdc.R
+++ NF-hichip-downstream/bin/loop_calling_hicdc.R
@@ -63,12 +63,14 @@ if(opt$verbose) print(opt)
     #             "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20")
     # chrs = c("chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28", "chr29", "chr30",
     #             "chr31", "chr32", "chr33", "chrZ", "chrW")
-    # chrs = c("chr29")
     # chrs = c("chr30", "chr31", "chr32", "chr33", "chrZ", "chrW")
-    chrs = c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
-             "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
-             "chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28",
-             "chr31", "chr32", "chr33", "chrZ", "chrW")
+
+    # chrs = c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
+    #          "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
+    #          "chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28",
+    #          "chr31", "chr32", "chr33", "chrZ", "chrW") ### THIS WORKS FOR ALL SAMPLES - ONLY ISSUES WITH 29 AND 30
+
+    chrs = c("chr29")
 
     opt$binsize = 5000
     opt$Dmin = 10000
