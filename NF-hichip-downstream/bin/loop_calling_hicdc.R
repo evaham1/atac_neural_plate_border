b999665efa5781866fd24ad234943e46b4e7f8dc
Eva <ehamrud@gmail.com>
2023-06-06 18:44:11 +0100

    debug try running all samples all chroms

--- NF-hichip-downstream/bin/loop_calling_hicdc.R
+++ NF-hichip-downstream/bin/loop_calling_hicdc.R
@@ -70,8 +70,6 @@ if(opt$verbose) print(opt)
     #          "chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28",
     #          "chr31", "chr32", "chr33", "chrZ", "chrW") ### THIS WORKS FOR ALL SAMPLES - ONLY ISSUES WITH 29 AND 30
 
-    chrs = c("chr30")
-
     opt$binsize = 5000
     opt$Dmin = 10000
     opt$Dmax = 1000000
