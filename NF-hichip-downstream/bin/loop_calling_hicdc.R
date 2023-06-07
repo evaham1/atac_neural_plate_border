5380da00b577da011f10e77eba0896ab1281e75a
Eva <ehamrud@gmail.com>
2023-06-07 14:20:10 +0100

    try running loop call and diffloops no chr30

--- NF-hichip-downstream/bin/loop_calling_hicdc.R
+++ NF-hichip-downstream/bin/loop_calling_hicdc.R
@@ -65,10 +65,10 @@ if(opt$verbose) print(opt)
     #             "chr31", "chr32", "chr33", "chrZ", "chrW")
     # chrs = c("chr30", "chr31", "chr32", "chr33", "chrZ", "chrW")
 
-    # chrs = c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
-    #          "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
-    #          "chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28",
-    #          "chr31", "chr32", "chr33", "chrZ", "chrW") ### THIS WORKS FOR ALL SAMPLES - ONLY ISSUES WITH 29 AND 30
+    chrs = c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
+             "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
+             "chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28",
+             "chr31", "chr32", "chr33", "chrZ", "chrW") ### THIS WORKS FOR ALL SAMPLES - ONLY ISSUES WITH chr30 (chr29 doesnt exist)
 
     opt$binsize = 5000
     opt$Dmin = 10000
