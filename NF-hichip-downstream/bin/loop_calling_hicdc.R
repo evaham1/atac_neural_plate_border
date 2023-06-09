dff4152656ce6a1b8888aa889f366b2697dcb994
Eva <ehamrud@gmail.com>
2023-06-09 09:54:47 +0100

    remove chrom15

--- NF-hichip-downstream/bin/loop_calling_hicdc.R
+++ NF-hichip-downstream/bin/loop_calling_hicdc.R
@@ -65,11 +65,15 @@ if(opt$verbose) print(opt)
     #             "chr31", "chr32", "chr33", "chrZ", "chrW")
     # chrs = c("chr30", "chr31", "chr32", "chr33", "chrZ", "chrW")
 
+    # chrs = c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
+    #          "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
+    #          "chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28",
+    #          "chr31", "chr32", "chr33", "chrZ", "chrW") ### THIS WORKS FOR ALL SAMPLES - ONLY ISSUES WITH chr30 (chr29 doesnt exist)
+             ### but now I've tried again after running nfclear and it doesn't work with chrom15???
     chrs = c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
-             "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
+             "chr11", "chr12", "chr13", "chr14", "chr16", "chr17", "chr18", "chr19", "chr20",
              "chr21", "chr22", "chr23", "chr24", "chr25", "chr26", "chr27", "chr28",
-             "chr31", "chr32", "chr33", "chrZ", "chrW") ### THIS WORKS FOR ALL SAMPLES - ONLY ISSUES WITH chr30 (chr29 doesnt exist)
-             ### but now I've tried again and it doesn't work with chrom15???
+             "chr31", "chr32", "chr33", "chrZ", "chrW") ### does this work?
 
     opt$binsize = 5000
     opt$Dmin = 0
