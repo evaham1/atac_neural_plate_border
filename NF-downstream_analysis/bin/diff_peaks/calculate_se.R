ce54070a825ddd829e9b5a64e7cdc46843ad7dda
Eva <ehamrud@gmail.com>
2023-06-21 09:15:31 +0100

    fix calculate_se script

--- NF-downstream_analysis/bin/diff_peaks/calculate_se.R
+++ NF-downstream_analysis/bin/diff_peaks/calculate_se.R
@@ -68,37 +68,6 @@ if(opt$verbose) print(opt)
 
 ############################### FUNCTIONS ####################################
 
-### Function to add unique ids to se peak object so can subset properly
-add_unique_ids_to_se <- function(seMarker, ArchR, matrix_type) {
-  
-  if (matrix_type == "PeakMatrix") {
-    tmp_peaks = data.frame(ArchR@peakSet)
-    tmp_diff_peaks = data.frame(rowData(seMarker))
-    diff_peaks_join_peakset = left_join(tmp_diff_peaks, tmp_peaks, 
-                                        by = c("seqnames" = "seqnames", "start" = "start", "end" = "end"))
-    diff_peaks_join_peakset$gene_name = paste(diff_peaks_join_peakset$nearestGene, diff_peaks_join_peakset$distToTSS,sep="_")
-    diff_peaks_join_peakset$unique_id = paste0(diff_peaks_join_peakset$seqnames, ":", diff_peaks_join_peakset$start, "-", diff_peaks_join_peakset$end)
-    rowData(seMarker) = diff_peaks_join_peakset
-  }
-  
-  if (matrix_type == "GeneScoreMatrix") {
-    rowData <- as.data.frame(rowData(seMarker))
-    
-    duplicated_gene_names <- rowData$name[duplicated(rowData$name)]
-    duplicated_genes <- rowData[which(rowData$name %in% duplicated_gene_names), ]
-    duplicated_genes <- duplicated_genes %>% group_by(name) %>% 
-      arrange(name) %>% mutate(unique_id = paste0(name, "-", rowid(name)))
-    join_df = left_join(rowData, duplicated_genes,
-                        by = c("seqnames" = "seqnames", "start" = "start", "end" = "end", "name" = "name", "idx" = "idx", "strand" = "strand"))
-    
-    join_df <- join_df %>% mutate(unique_id = coalesce(unique_id, name))
-    
-    rowData(seMarker) = join_df
-  }
-  
-  return(seMarker)
-}
-
 ### function to turn summarised experiment object into a df that can be printed to see diff features
 extract_features_table <- function(markersPeaks) {
   markerList <- getMarkers(markersPeaks, cutOff = "FDR <= 1") # keep all peaks
@@ -148,12 +117,12 @@ se <- getMarkerFeatures(
   ArchRProj = ArchR, 
   useMatrix = "PeakMatrix", 
   groupBy = opt$group_by)
-se <- add_unique_ids_to_se(se, ArchR, matrix_type = "PeakMatrix")
+se <- scHelper::ArchRAddUniqueIdsToSe(se, ArchR, matrix_type = "PeakMatrix")
 
 saveRDS(se, file = paste0(rds_path, label, "_SE.RDS"))
 
 ###########################################################################################
 ########################## Extract table of differentially accessible peaks ###############################
 
-peaks_table <- extract_features_table(markersPeaks)
+peaks_table <- extract_features_table(se)
 write.csv(peaks_table, paste0(plot_path, "all_differentially_accessible_peaks.csv"), row.names = FALSE)
