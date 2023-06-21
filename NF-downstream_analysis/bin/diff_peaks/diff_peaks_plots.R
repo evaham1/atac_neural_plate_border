d51b9d257727e7a9c3ccdacbd5fbfe18ccb0f5f8
Eva <ehamrud@gmail.com>
2023-06-21 15:46:04 +0100

    update diff peaks plots

--- NF-downstream_analysis/bin/diff_peaks/diff_peaks_plots.R
+++ NF-downstream_analysis/bin/diff_peaks/diff_peaks_plots.R
@@ -26,7 +26,8 @@ library(scHelper)
 option_list <- list(
   make_option(c("-r", "--runtype"), action = "store", type = "character", help = "Specify whether running through through 'nextflow' in order to switch paths"),
   make_option(c("-c", "--cores"), action = "store", type = "integer", help = "Number of CPUs"),
-  make_option(c("", "--verbose"), action = "store", type = "logical", help = "Verbose", default = FALSE)
+  make_option(c("-g", "--group_by"), action = "store", type = "character", help = "How to group cells to call peaks", default = "clusters",),
+  make_option(c("", "--verbose"), action = "store", type = "logical", help = "Verbose", default = TRUE)
 )
 
 opt_parser = OptionParser(option_list = option_list)
@@ -40,9 +41,16 @@ if(opt$verbose) print(opt)
     
     ncores = 8
     
-    # test input folder made
-    data_path = "./output/NF-downstream_analysis/ArchR_integration/transfer_labels/peak_call/rds_files/"
-    plot_path = "./output/NF-downstream_analysis/ArchR_integration/transfer_labels_late_peaks/plots/"
+    # input folder - none of these work
+    data_path = "./output/NF-downstream_analysis/Processing/ss8/ARCHR_INTEGRATING_WF/Single_cell_integration/rds_files/"
+    data_path = "./output/NF-downstream_analysis/Processing/HH6/ARCHR_INTEGRATING_WF/Single_cell_integration/rds_files/"
+    data_path = "./output/NF-downstream_analysis/Processing/HH7/ARCHR_INTEGRATING_WF/Single_cell_integration/rds_files/"
+    data_path = "./output/NF-downstream_analysis/Processing/ss4/ARCHR_INTEGRATING_WF/Single_cell_integration/rds_files/"
+    data_path = "./output/NF-downstream_analysis/Processing/ss4/ARCHR_INTEGRATING_WF/Single_cell_integration_cluster_identification/rds_files/"
+    data_path = "./output/NF-downstream_analysis/Processing/ss8/Peak_call/rds_files/"
+    
+    # works but no peaks have to recalculate interactively
+    data_path = "./output/NF-downstream_analysis/Processing/ss8/Clustering/rds_files/"
     
     addArchRThreads(threads = 1) 
     
@@ -93,8 +101,8 @@ print(ArchR)
 # check that gene score matrix and gene integration matrix are available
 getAvailableMatrices(ArchRProj = ArchR)
 
-# read in se object
-se <- readRDS(paste0(data_path, label, "_SE.RDS"))
+# read in se object
+seMarker <- readRDS(paste0(data_path, label, "_SE.RDS"))
 
 print("data read in!")
 
@@ -124,112 +132,175 @@ names(scHelper_cell_type_colours) <- c('NNE', 'HB', 'eNPB', 'PPR', 'aPPR', 'stre
 # set colour palettes for UMAPs
 atac_scHelper_old_cols <- scHelper_cell_type_colours[unique(ArchR$scHelper_cell_type_old)]
 
-# set pal for heatmaps
+# set pal for heatmaps
 pal = paletteContinuous(set = "solarExtra", n = 100)
 
-##########################################################################
-############################## PLOTS #####################################
 
-##### Prepare data for plotting 
-matrix <- scHelper::ArchR_ExtractMeansFromSe(se) # extract means df from se object
-normalised_matrix <- Log2norm(matrix) # log2norm across all features in each cell group
+##############################################################################
+############################# Peak Heatmaps ##################################
+
+plot_path_temp <- paste0(plot_path, "diff_peaks_heatmaps/")
+dir.create(plot_path_temp, recursive = T)
+  
+# prepare for plotting
+normalised_matrix <- ArchR_ExtractMeansFromSe(seMarker, Log2norm = TRUE, scaleTo = 10^4) # extract means df from se object and log2norm all features in each cell group
+print("matrix for plotting made")
+  
+# Heatmap of positive markers which pass cutoff thresholds
+ids <- ArchR_ExtractIds(seMarker, cutOff = "FDR <= 0.01 & Log2FC >= 1", top_n = FALSE) # extract ids
+if (length(ids) < 2){
+  print(paste0(length(ids), " features passed cutoff - not enough to make heatmap"))
+} else {
+  print(paste0(length(ids), " features passed cutoff - now plotting heatmap"))
+  subsetted_matrix <- normalised_matrix[ids, ]
+    
+  png(paste0(plot_path_temp, 'diff_cutoff_heatmap.png'), height = 40, width = 20, units = 'cm', res = 400)
+  print(ArchR_PlotMarkerHeatmap(subsetted_matrix, pal = pal, fontSizeCols = 20))
+  graphics.off()
+}
+  
+# Heatmap of positive markers top 10 per cell group
+ids <- ArchR_ExtractIds(seMarker, cutOff = "FDR <= 0.05 & Log2FC >= 0", top_n = TRUE, n = 10) # extract ids
+print(paste0(length(ids), " features passed cutoff for top 10 heatmap"))
+subsetted_matrix <- normalised_matrix[ids, ]
+  
+png(paste0(plot_path_temp, 'diff_top10_heatmap.png'), height = 70, width = 20, units = 'cm', res = 400)
+ArchR_PlotMarkerHeatmap(subsetted_matrix, labelRows = TRUE, pal = pal, cluster_columns = FALSE, cluster_rows = FALSE, fontSizeCols = 20)
+graphics.off()
+
+print("heatmaps made")
+
+############################# Promoter Peaks #######################################
+
+# extract diff peaks that are promoters
+markersPeaks_promoter <- subset(seMarker, rowData(seMarker)$peakType == "Promoter")
+marker_tables_promoter = markersPeaks_promoter %>% getMarkers(cutOff = "FDR <= 0.01 & Log2FC >= 1")
+
+# make df of them and write
+marker_tables_promoter_tmp = marker_tables_promoter %>%  as.data.frame()
+write.csv(marker_tables_promoter_tmp, paste0(plot_path, "differentially_accessible_peaks_promoter.csv"), row.names = FALSE)
+
+# plot them in heatmap
+ids <- marker_tables_promoter_tmp$unique_id
+subsetted_matrix <- normalised_matrix[ids, ]
+dim(subsetted_matrix)
+
+png(paste0(plot_path_temp, 'diff_cutoff_promoter_peaks_heatmap.png'), height = 40, width = 20, units = 'cm', res = 400)
+ArchR_PlotMarkerHeatmap(subsetted_matrix, pal = pal, labelRows = TRUE, fontSizeCols = 20)
+graphics.off()
+
+############################# Distal Peaks #######################################
+
+# extract diff peaks that are promoters
+markersPeaks_distal <- subset(seMarker, rowData(seMarker)$peakType == "Distal")
+marker_tables_distal = markersPeaks_distal %>% getMarkers(cutOff = "FDR <= 0.01 & Log2FC >= 1")
+
+# make df of them and write
+marker_tables_distal_tmp = marker_tables_distal %>%  as.data.frame()
+write.csv(marker_tables_distal_tmp, paste0(plot_path, "differentially_accessible_peaks_distal.csv"), row.names = FALSE)
+
+# plot them in heatmap
+ids <- marker_tables_distal_tmp$unique_id
+subsetted_matrix <- normalised_matrix[ids, ]
+dim(subsetted_matrix)
+
+png(paste0(plot_path_temp, 'diff_cutoff_distal_peaks_heatmap.png'), height = 40, width = 20, units = 'cm', res = 400)
+ArchR_PlotMarkerHeatmap(subsetted_matrix, pal = pal, labelRows = TRUE, fontSizeCols = 20)
+graphics.off()
 
 
-# ##### Plot all differential peaks
-# ids <- scHelper::ArchR_ExtractIds(se, cutOff = "FDR <= 0.01 & Log2FC >= 1", top_n = FALSE)
-# print(paste0("all peaks: ", length(ids)))
+###################### Boxplots showing distribution of FDR and Logf2c values #############################
 
-# if (length(ids) > 4){
-#   matrix <- ArchR_ExtractMeansFromSe(se)
-#   normalised_matrix <- Log2norm(matrix)
-#   subsetted_matrix <- subset_matrix(normalised_matrix, ids)
+png(paste0(plot_path, 'Log2FC_boxplot.png'), height = 20, width = 20, units = 'cm', res = 400)
+boxplot(assays(seMarker)$Log2FC)
+graphics.off()
 
-#   png(paste0(plot_path, 'full_heatmap.png'), height = 70, width = 60, units = 'cm', res = 400)
-#   print(scHelper::ArchR_PlotMarkerHeatmap(subsetted_matrix, pal = pal, clusterCols = FALSE))
-#   graphics.off()
-# }
-
-# ids <- scHelper::ArchR_ExtractIds(se, cutOff = "FDR <= 0.05 & Log2FC >= 0", top_n = TRUE)
-# print(paste0("all peaks top 10: ", length(ids)))
-
-# if (length(ids) > 4){
-#   matrix <- scHelper::ArchR_ExtractMeansFromSe(se)
-#   normalised_matrix <- Log2norm(matrix)
-#   subsetted_matrix <- subset_matrix(normalised_matrix, ids)
-
-#   png(paste0(plot_path, 'full_heatmap_top10.png'), height = 40, width = 30, units = 'cm', res = 400)
-#   print(scHelper::ArchR_PlotMarkerHeatmap(subsetted_matrix, pal = pal, clusterCols = FALSE, labelRows = TRUE))
-#   graphics.off()
-# }
-
-# print(normalised_matrix[1:2, 1:2])
-# print(head(se))
+png(paste0(plot_path, 'FDR_boxplot.png'), height = 20, width = 20, units = 'cm', res = 400)
+boxplot(assays(seMarker)$FDR)
+graphics.off()
 
 
-# ############################# Add peak information to markersPeaks object #######################################
+###################### Extract Log2FC and p-vals of significant peaks from each cell group #############################
 
-# tmp_peaks = data.frame(ArchR@peakSet)
-# tmp_diff_peaks = data.frame(rowData(se))
+group_df <- getCellColData(ArchR, select = opt$group_by)
+groups <- unique(group_df[,1])
 
-# print(head(tmp_diff_peaks))
+print("Looping over cell groups:")
+print(groups)
 
-# diff_peaks_join_peakset = left_join(tmp_diff_peaks, tmp_peaks, by = c("seqnames" = "seqnames", "start" = "start", "end" = "end"))
-# diff_peaks_join_peakset$name = paste(diff_peaks_join_peakset$nearestGene, diff_peaks_join_peakset$distToTSS,sep="_")
-# diff_peaks_join_peakset$unique_id = paste(diff_peaks_join_peakset$seqnames, diff_peaks_join_peakset$start, diff_peaks_join_peakset$end, sep=":")
-# rowData(se) = diff_peaks_join_peakset
+final_df <- data.frame()
 
-# ############################# Promoter Peaks #######################################
+for (group in groups){
+  
+  print(group)
+  
+  # extract peaks from this group using pseudoreplicate names
+  length(grep(group, rowData(seMarker)$GroupReplicate))
+  length(rowData(seMarker)$GroupReplicate)
+  
+  subset_se <- seMarker[grep(group, rowData(seMarker)$GroupReplicate), ]
+  
+  # extract logFC and p-vals for these peaks and put in df
+  df <- data.frame(LogFC = c(t(assays(subset_se)$Log2FC)), FDR = c(t(assays(subset_se)$FDR)),
+                   LogFDR = log10(c(t(assays(subset_se)$FDR))), stringsAsFactors=FALSE)
+  df <- df %>% mutate(Passed = as.factor(ifelse(FDR < 0.01 & LogFC > 1 | FDR < 0.01 & LogFC < -1,"passed", "failed")))
+  # add group name to this df
+  df <- df %>% mutate(Cell_group = group)
+  
+  # combine to final df
+  final_df <- rbind(final_df, df)
+}
 
-# markersPeaks_promoter <- subset(se, rowData(se)$peakType == "Promoter")
-# marker_tables_promoter = markersPeaks_promoter %>% getMarkers(cutOff = "FDR <= 0.1 & Log2FC >= 0.5")
-# marker_tables_promoter_tmp = marker_tables_promoter %>%  as.data.frame()
-# write.csv(marker_tables_promoter_tmp, paste0(plot_path, "all_differentially_expressed_peaks_promoter.csv"), row.names = FALSE)
+###################### Volcano plots #############################
 
-# heatmapPeaks <- plotMarkerHeatmap(
-#   seMarker = markersPeaks_promoter,
-#   cutOff = "FDR <= 0.3 & Log2FC >= 0.5",
-#   nLabel = 3)
-# png(paste0(plot_path, 'diff_promoter_peak_cutoff_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
-# draw(heatmapPeaks, heatmap_legend_side = "bot", annotation_legend_side = "bot")
-# graphics.off()
+set.seed(42)
+rows <- sample(nrow(final_df))
+final_df <- final_df[rows, ]
 
-# ############################# Distal Peaks #######################################
+png(paste0(plot_path, 'FDR_Log2FC_scatterplot_sig_colour.png'), height = 23, width = 20, units = 'cm', res = 400)
+ggplot(final_df, aes(x = -LogFDR, y = LogFC, color = Passed, shape = Passed)) +
+  geom_point() +
+  scale_color_manual(values=c("black", "4f7942")) +
+  scale_shape_manual(values=c(16, 17)) +
+  xlim(0, 50) +
+  ylim(-15, 15) +
+  theme_minimal()
+graphics.off()
 
-# markersPeaks_distal <- subset(se, rowData(se)$peakType == "Distal")
-# marker_tables_distal = markersPeaks_distal %>% getMarkers(cutOff = "FDR <= 0.1 & Log2FC >= 0.5")
-# marker_tables_distal_tmp = marker_tables_distal %>%  as.data.frame()
-# write.csv(marker_tables_distal_tmp, paste0(plot_path, "all_differentially_expressed_peaks_distal.csv"), row.names = FALSE)
+png(paste0(plot_path, 'FDR_Log2FC_scatterplot_cell_group_col.png'), height = 23, width = 20, units = 'cm', res = 400)
+ggplot(final_df, aes(x = -LogFDR, y = LogFC, color = Cell_group, shape = Passed)) +
+  geom_point() +
+  # scale_color_manual(values=c("black", "4f7942")) +
+  scale_shape_manual(values=c(16, 17)) +
+  xlim(0, 50) +
+  ylim(-15, 15) +
+  theme_minimal()
+graphics.off()
 
-# heatmapPeaks <- plotMarkerHeatmap(
-#   seMarker = markersPeaks_distal,
-#   cutOff = "FDR <= 0.3 & Log2FC >= 0.5",
-#   nLabel = 3)
-# png(paste0(plot_path, 'diff_distal_peak_cutoff_heatmap.png'), height = 50, width = 40, units = 'cm', res = 400)
-# draw(heatmapPeaks, heatmap_legend_side = "bot", annotation_legend_side = "bot")
-# graphics.off()
+for (group in groups){
+  print(group)
+  
+  # add temp column to df to only highlight these peaks
+  group_df <- final_df %>% mutate(Highlight = as.factor(ifelse(Cell_group == group,"passed", "failed")))
+  
+  # plot with highlight on this cell group
+  png(paste0(plot_path, 'FDR_Log2FC_scatterplot_cell_group_col_', group, '.png'), height = 23, width = 20, units = 'cm', res = 400)
+  print(
+    ggplot(group_df, aes(x = -LogFDR, y = LogFC, color = Highlight, shape = Passed)) +
+    geom_point() +
+    scale_color_manual(values=c("grey", "red")) +
+    scale_shape_manual(values=c(16, 17)) +
+    xlim(0, 50) +
+    ylim(-15, 15) +
+    theme_minimal()
+  )
+  graphics.off()
+}
 
-# ###################### Plots showing distribution of FDR and Logf2c values #############################
+###################### Barcharts of nPeaks per cluster and how many are significant #############################
 
-# png(paste0(plot_path, 'Log2FC_boxplot.png'), height = 20, width = 20, units = 'cm', res = 400)
-# boxplot(assays(se)$Log2FC)
-# graphics.off()
+freq_table <- table(final_df$Cell_group, final_df$Passed)
 
-# png(paste0(plot_path, 'FDR_boxplot.png'), height = 20, width = 20, units = 'cm', res = 400)
-# boxplot(assays(se)$FDR)
-# graphics.off()
-
-# df <- data.frame(LogFC = c(t(assays(se)$Log2FC)), FDR = c(t(assays(se)$FDR)), 
-#                  LogFDR = log10(c(t(assays(se)$FDR))), stringsAsFactors=FALSE)
-
-# df <- df %>% mutate(Passed = as.factor(ifelse(FDR < 0.01 & LogFC > 1,"passed", "failed")))
-
-# set.seed(42)
-# rows <- sample(nrow(df))
-# df <- df[rows, ]
-
-# png(paste0(plot_path, 'FDR_Log2FC_scatterplot.png'), height = 23, width = 20, units = 'cm', res = 400)
-# ggplot(df, aes(x = -LogFDR, y = LogFC, color = Passed, shape = Passed)) + 
-#   geom_point() + 
-#   scale_color_manual(values=c("black", "red")) +
-#   scale_shape_manual(values=c(16, 17))
-# graphics.off()
+png(paste0(plot_path, 'Freq_peaks_passed_by_cell_group.png'), height = 23, width = 20, units = 'cm', res = 400)
+plot(freq_table)
+graphics.off()
