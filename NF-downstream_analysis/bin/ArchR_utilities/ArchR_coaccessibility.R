73874f8c91885703b152a42ab87ffd39c245cf92
Eva <ehamrud@gmail.com>
2023-07-10 10:12:17 +0100

    init archr co-accessibility script

---
+++ NF-downstream_analysis/bin/ArchR_utilities/ArchR_coaccessibility.R
@@ -0,0 +1,88 @@
+#!/usr/bin/env Rscript
+
+print("coaccessibility ArchR")
+
+############################## Load libraries #######################################
+library(getopt)
+library(optparse)
+library(ArchR)
+library(tidyverse)
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(GenomicFeatures)
+library(parallel)
+library(scHelper)
+
+############################## Set up script options #######################################
+# Read in command line opts
+option_list <- list(
+    make_option(c("-r", "--runtype"), action = "store", type = "character", help = "Specify whether running through through 'nextflow' in order to switch paths"),
+    make_option(c("-c", "--cores"), action = "store", type = "integer", help = "Number of CPUs"),
+    make_option(c("", "--verbose"), action = "store", type = "logical", help = "Verbose", default = FALSE)
+    )
+
+opt_parser = OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+if(opt$verbose) print(opt)
+
+# Set paths and load data
+{
+  if(length(commandArgs(trailingOnly = TRUE)) == 0){
+    cat('No command line arguments provided, paths are set for running interactively in Rstudio server\n')
+    
+    ncores = 8
+    
+    # already clustered
+    data_path = "./output/NF-downstream_analysis/Processing/FullData/Clustering/rds_files/"
+    ## smaller object
+    data_path = "./output/NF-downstream_analysis/Processing/ss8/Clustering/rds_files/"
+    
+    addArchRThreads(threads = 1) 
+    
+  } else if (opt$runtype == "nextflow"){
+    cat('pipeline running through Nextflow\n')
+    
+    plot_path = "./plots/"
+    rds_path = "./rds_files/"
+    data_path = "./input/rds_files/"
+    ncores = opt$cores
+    
+    addArchRThreads(threads = ncores)
+    
+  } else {
+    stop("--runtype must be set to 'nextflow'")
+  }
+  
+  cat(paste0("script ran with ", ncores, " cores\n")) 
+  dir.create(plot_path, recursive = T)
+  dir.create(rds_path, recursive = T)
+}
+
+############################## Read in ArchR project #######################################
+
+# If files are not in rds_files subdirectory look in input dir 
+label <- unique(sub('_.*', '', list.files(data_path)))
+print(label) 
+
+if (length(label) == 0){
+  data_path = "./input/"
+  label <- sub('_.*', '', list.files(data_path))
+  print(label)
+  ArchR <- loadArchRProject(path = paste0(data_path, label, "_Save-ArchR"), force = FALSE, showLogo = TRUE)
+  paste0("Memory Size = ", round(object.size(ArchR) / 10^6, 3), " MB")
+} else {
+  ArchR <- loadArchRProject(path = paste0(data_path, label, "_Save-ArchR"), force = FALSE, showLogo = TRUE)
+  paste0("Memory Size = ", round(object.size(ArchR) / 10^6, 3), " MB")
+}
+
+# see what is in the ArchR object already
+print("ArchR object info: ")
+print(ArchR)
+getPeakSet(ArchR)
+getAvailableMatrices(ArchR)
+
+
+#################################################################################
+############################## CO-ACCESSIBILITY #######################################
+
