67ce29323ebfe6ab0d6f8bd213998d32e7eb623c
Eva <ehamrud@gmail.com>
2023-10-06 16:24:29 +0100

    typo

--- NF-downstream_analysis/bin/scMEGA/MEGA_GRNi.R
+++ NF-downstream_analysis/bin/scMEGA/MEGA_GRNi.R
@@ -803,7 +803,7 @@ graphics.off()
 
 # plot TF-gene correlation heatmap
 df.tf.gene <- GetTFGeneCorrelation_updated(object = obj.traj,
-                                           tf.use = df.tfs$tfs,
+                                           tf.use = df.grn$tf,
                                            gene.use = unique(df.grn$gene),
                                            tf.assay = "chromvar",
                                            gene.assay = "RNA",
@@ -901,7 +901,7 @@ graphics.off()
 
 # plot TF-gene correlation heatmap
 df.tf.gene <- GetTFGeneCorrelation_updated(object = obj.traj,
-                                           tf.use = df.tfs$tfs,
+                                           tf.use = df.grn.pos$tf,
                                            gene.use = unique(df.grn.pos$gene),
                                            tf.assay = "chromvar",
                                            gene.assay = "RNA",
