52fd3b4876f3e8e2367f54d12d5606ff1372d1b3
Eva <ehamrud@gmail.com>
2023-10-14 10:17:11 +0100

    fix pipeline switches

--- NF-downstream_analysis/run_scripts/crick_full_metacell_processing.sh
+++ NF-downstream_analysis/run_scripts/crick_full_metacell_processing.sh
@@ -20,9 +20,9 @@ ml Graphviz
 nextflow run ./main.nf \
 --outdir ../output/NF-downstream_analysis \
 --skip_upstream_processing true \
---skip_peakcall_processing true \
---skip_singlecell_processing true \
+--skip_sc_processing true \
 --skip_metacell_processing false \
 --skip_multiview_processing true \
+--skip_mega_processing true \
 -profile crick_full \
 -resume
