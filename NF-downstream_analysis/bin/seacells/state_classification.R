9f1240680498bf92f49daaa0e764b0d715c08b10
Eva <ehamrud@gmail.com>
2023-11-19 08:58:54 +0000

    res

--- NF-downstream_analysis/bin/seacells/state_classification.R
+++ NF-downstream_analysis/bin/seacells/state_classification.R
@@ -211,8 +211,8 @@ print(paste0("Stage: ", stage))
 
 if(length(stage) == 1){
   cell_state_markers = cell_state_markers[[stage]]
-  cluster_res = list(HH5 = 2.4, HH6 = 2.2, HH7 = 3.1, ss4 = 3.2, ss8 = 3.3)[[stage]]
-  # cluster res set so same number cell states found in sc: 11 at HH5, 10 at HH6, 14 at HH7, 13 at 4ss and 13 at 8ss
+  cluster_res = list(HH5 = 2.4, HH6 = 2.2, HH7 = 3.3, ss4 = 3.2, ss8 = 3.5)[[stage]]
+  # cluster res set so same number cell states found in sc: 11 at HH5 D, 10 at HH6 D, 14 at HH7, 13 at 4ss D and 13 at 8ss
   metadata = c('scHelper_cell_type')
 } else {
   cell_state_markers = flatten(cell_state_markers)
