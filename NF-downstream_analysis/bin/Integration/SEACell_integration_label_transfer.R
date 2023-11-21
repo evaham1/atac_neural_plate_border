a43177ab3a480a022f54ce7e79d8f49634e32d6d
Eva <ehamrud@gmail.com>
2023-11-21 14:36:50 +0000

    debug

--- NF-downstream_analysis/bin/Integration/SEACell_integration_label_transfer.R
+++ NF-downstream_analysis/bin/Integration/SEACell_integration_label_transfer.R
@@ -366,10 +366,15 @@ colnames(map1)[colnames(map1) == 'scHelper_cell_type_by_proportion'] <- 'scHelpe
 row.names(map1) <- NULL  # Clear existing row names
 row.names(map1) <- 1:nrow(map1)  # Set row names to unique numbers
 head(map1)
+rownames(map1)
 map2 <- rownames_to_column(seurat@meta.data, var = "ATAC")
 row.names(map2) <- NULL  # Clear existing row names
 row.names(map2) <- 1:nrow(map2)  # Set row names to unique numbers
+head(map2)
+rownames(map2)
 map <- merge(map1, map2, by = "ATAC", all = TRUE)
+head(map)
+rownames(map)
 row.names(map) <- NULL  # Clear existing row names
 row.names(map) <- 1:nrow(map)  # Set row names to unique numbers
 metadata <- column_to_rownames(map, var = "ATAC")
